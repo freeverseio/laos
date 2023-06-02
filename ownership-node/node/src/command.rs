740aaa3f14f1bc4d2f14957cfd3690b06a314206
magecnion <magecnion@gmail.com>
2023-06-02 11:13:53 +0200

    add arrakis chain spec

--- ownership-node/node/src/command.rs
+++ ownership-node/node/src/command.rs
@@ -22,6 +22,7 @@ use crate::{
 
 fn load_spec(id: &str) -> std::result::Result<Box<dyn ChainSpec>, String> {
 	Ok(match id {
+		"arrakis" => Box::new(chain_spec::ChainSpec::from_json_bytes(&include_bytes!("../../specs/arrakis.json")[..])?),
 		"dev" => Box::new(chain_spec::development_config()),
 		"template-rococo" => Box::new(chain_spec::local_testnet_config()),
 		"" | "local" => Box::new(chain_spec::local_testnet_config()),
