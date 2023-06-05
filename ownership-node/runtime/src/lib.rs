d9950d032449863522abe9ff544bce2d6a52a427
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2023-06-05 17:52:05 +0200

    integrate pallet_livingassets_ownership

--- ownership-node/runtime/src/lib.rs
+++ ownership-node/runtime/src/lib.rs
@@ -58,7 +58,7 @@ use xcm::latest::prelude::BodyId;
 use xcm_executor::XcmExecutor;
 
 /// Import the template pallet.
-pub use pallet_template;
+pub use pallet_livingassets_ownership;
 
 /// Alias to 512-bit hash when used in the context of a transaction signature on the chain.
 pub type Signature = MultiSignature;
@@ -456,8 +456,7 @@ impl pallet_collator_selection::Config for Runtime {
 	type WeightInfo = ();
 }
 
-/// Configure the pallet template in pallets/template.
-impl pallet_template::Config for Runtime {
+impl pallet_livingassets_ownership::Config for Runtime {
 	type RuntimeEvent = RuntimeEvent;
 }
 
@@ -492,7 +491,7 @@ construct_runtime!(
 		DmpQueue: cumulus_pallet_dmp_queue = 33,
 
 		// Template
-		TemplatePallet: pallet_template = 40,
+		LivingassetsOwnership: pallet_livingassets_ownership = 40,
 	}
 );
 
