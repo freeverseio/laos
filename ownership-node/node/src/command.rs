75786aa0f78eb103e4cc06b4d1730e17fa8590e2
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2023-07-13 18:17:05 +0200

    new chain specs (#47)
    * new chain specs
    
    * set sudo and soem freeverse accounts
    
    * set SS58 accounts
    
    * setting arrakis params
    
    * fix relay chain specification
    
    * adding docker infra to build images
    
    * renamed
    
    * fix specs
    
    * first bootnode
    
    * add bootnode
    
    * new indentities
    
    * new parachain id
    
    * new bootnodes
    
    * using raw
    
    * bootnodes
    
    * fix file coping
    
    * unmask the binary
    
    * using the fast build
    
    * clippy with release
    
    * cleaning
    
    * fix local dockerfile path
    
    * add release job
    
    * local dockerfile
    
    * new dockerfile
    
    * new chain 2667
    
    * 1 collator
    
    * rename
    
    * delete
    
    * set bootnodes identity
    
    * set ips of bootnodes
    
    * add lint job
    
    * build doesn't lint
    
    * renaming jib
    
    * renaming job
    
    * using parallel jobs
    
    * using parametrized job name
    
    * make test inside the build job
    
    * rename job
    
    * using Laos-runners
    
    * using 8 cores
    
    * using 8 cores
    
    * back to normal
    
    * using 8 cores
    
    * using labels
    
    * using 16 cores
    
    * using runners
    
    * remove unused chain_specs

--- ownership-node/node/src/command.rs
+++ ownership-node/node/src/command.rs
@@ -26,7 +26,7 @@ use crate::{
 fn load_spec(id: &str) -> std::result::Result<Box<dyn ChainSpec>, String> {
 	Ok(match id {
 		"arrakis" => Box::new(chain_spec::ChainSpec::from_json_bytes(
-			&include_bytes!("../../specs/arrakis.json")[..],
+			&include_bytes!("../../specs/arrakis-frontier.json")[..],
 		)?),
 		"dev" => Box::new(chain_spec::development_config()),
 		"template-rococo" => Box::new(chain_spec::local_testnet_config()),
