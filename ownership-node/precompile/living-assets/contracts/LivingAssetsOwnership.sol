23b9c5d59428719a52aa58272052d6dd7ef83898
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2023-08-03 14:15:13 +0200

    removed legacy code from solidity interface

--- ownership-node/precompile/living-assets/contracts/LivingAssetsOwnership.sol
+++ ownership-node/precompile/living-assets/contracts/LivingAssetsOwnership.sol
@@ -6,14 +6,8 @@ pragma solidity >=0.8.3;
 /// @dev The interface through which solidity contracts will interact with pallet-living-assets
 /// @custom:address 0x0000000000000000000000000000000000000402
 interface LivingAssets {
-    /// @dev Create collection
-    /// @param collection_id The `collection_id` to be associated
-    function createCollection(
-        uint64 collection_id,
-        address who
-    ) external;
-
-    /// @dev Create collection 2
+    /// @dev create collection
+    /// @return collection_id The `collection_id`
     function createCollection() external returns (uint64);
 
     /// @dev Get collection owner
