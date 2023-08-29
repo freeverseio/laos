d7ea98c43d0c4214f39410a8803f5985828871b4
dastan <137785454+dastanfv@users.noreply.github.com>
2023-08-29 17:17:25 +0300

    Move laos relay from `parity-bridges-common` (#2)
    * Add laos relay
    
    * Update comment
    
    * Fix fmt check
    
    * Change name of the binary
    
    * install wasm32 toolkit
    
    ---------
    
    Co-authored-by: Alessandro Siniscalchi <asiniscalchi@gmail.com>

---
+++ bridge/src/bridges/ownership_parachain_evochain/evochain_headers_to_ownership_parachain.rs
@@ -0,0 +1,55 @@
+// Copyright 2019-2021 Parity Technologies (UK) Ltd.
+// This file is part of Parity Bridges Common.
+
+// Parity Bridges Common is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// Parity Bridges Common is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with Parity Bridges Common.  If not, see <http://www.gnu.org/licenses/>.
+
+//! Evochain-to-OwnershipParachain headers sync entrypoint.
+use crate::cli::bridge::{CliBridgeBase, RelayToRelayHeadersCliBridge};
+use substrate_relay_helper::{
+    finality::SubstrateFinalitySyncPipeline,
+    finality_base::{engine::Grandpa as GrandpaFinalityEngine, SubstrateFinalityPipeline},
+};
+substrate_relay_helper::generate_submit_finality_proof_call_builder!(
+    EvochainFinalityToOwnershipParachain,
+    EvochainFinalityToOwnershipParachainCallBuilder,
+    relay_laos_ownership_client::RuntimeCall::BridgeEvochainGrandpa,
+    relay_laos_ownership_client::BridgeGrandpaCall::submit_finality_proof
+);
+
+/// Description of Evochain -> Rococo finalized headers bridge.
+#[derive(Clone, Debug)]
+pub struct EvochainFinalityToOwnershipParachain;
+
+impl SubstrateFinalityPipeline for EvochainFinalityToOwnershipParachain {
+    type SourceChain = relay_laos_evolution_client::Evochain;
+    type TargetChain = relay_laos_ownership_client::OwnershipParachain;
+
+    type FinalityEngine = GrandpaFinalityEngine<Self::SourceChain>;
+}
+
+impl SubstrateFinalitySyncPipeline for EvochainFinalityToOwnershipParachain {
+    type SubmitFinalityProofCallBuilder = EvochainFinalityToOwnershipParachainCallBuilder;
+}
+
+//// `Evochain` to `OwnershipParachain`  bridge definition.
+pub struct EvochainToOwnershipParachainCliBridge {}
+
+impl CliBridgeBase for EvochainToOwnershipParachainCliBridge {
+    type Source = relay_laos_evolution_client::Evochain;
+    type Target = relay_laos_ownership_client::OwnershipParachain;
+}
+
+impl RelayToRelayHeadersCliBridge for EvochainToOwnershipParachainCliBridge {
+    type Finality = EvochainFinalityToOwnershipParachain;
+}
--- bridge/src/main.rs
+++ bridge/src/main.rs
@@ -1,3 +1,13 @@
+//! LAOS bridge relay entrypoint.
+
+#![warn(missing_docs)]
+
+mod bridges;
+mod chains;
+mod cli;
+
 fn main() {
-    println!("Hello, world!");
+    let command = cli::parse_args();
+    let run = command.run();
+    async_std::task::block_on(run);
 }
