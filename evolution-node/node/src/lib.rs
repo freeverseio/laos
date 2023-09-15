826e056522c62dc151fd5268d88d4c8e400ebc86
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2023-09-15 19:37:13 +0200

    simplify

--- evolution-node/node/src/lib.rs
+++ evolution-node/node/src/lib.rs
@@ -1,14 +1,2 @@
-mod chain_spec;
-#[macro_use]
-mod service;
-mod cli;
-mod command;
-mod benchmarking;
-
-/// Node run result.
-pub type Result = sc_cli::Result<()>;
-
-/// Run node.
-pub fn run() -> Result {
-	command::run()
-}
+pub mod chain_spec;
+pub mod service;
