870df3c7b00aa6105fe354b8b8152d9e37a9494a
magecnion <magecnion@gmail.com>
2023-09-26 12:20:02 +0200

    comments

--- ownership-node/pallets/living-assets-ownership/src/lib.rs
+++ ownership-node/pallets/living-assets-ownership/src/lib.rs
@@ -100,7 +100,7 @@ pub mod pallet {
 		CollectionCreated { collection_id: CollectionId, who: T::AccountId },
 		/// Asset transferred to `who`
 		/// parameters. [collection_id, asset_id, who]
-		AssetTransferred { collection_id: CollectionId, asset_id: U256, receiver: T::AccountId },
+		AssetTransferred { collection_id: CollectionId, asset_id: U256, to: T::AccountId },
 	}
 
 	// Errors inform users that something went wrong.
@@ -195,7 +195,7 @@ pub mod pallet {
 
 			let to = T::H160ToAccountId::convert(to.clone());
 			AssetOwner::<T>::set(collection_id, asset_id, Some(to.clone()));
-			Self::deposit_event(Event::AssetTransferred { collection_id, asset_id, receiver: to });
+			Self::deposit_event(Event::AssetTransferred { collection_id, asset_id, to });
 
 			Ok(())
 		}
