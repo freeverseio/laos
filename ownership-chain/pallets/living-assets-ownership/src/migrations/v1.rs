2d4571bed475b56aa0eb7283b13e24e0a08abc69
magecnion <magecnion@gmail.com>
2023-10-03 09:03:14 +0200

    use clear_storage_prefix and add version control to migration

--- ownership-chain/pallets/living-assets-ownership/src/migrations/v1.rs
+++ ownership-chain/pallets/living-assets-ownership/src/migrations/v1.rs
@@ -1,34 +1,17 @@
 use crate::{Config, Pallet};
 use frame_support::{
-	log, storage_alias,
-	traits::{Get, OnRuntimeUpgrade},
+	log,
+	traits::{Get, GetStorageVersion, OnRuntimeUpgrade, PalletInfoAccess, StorageVersion},
 };
 
-#[cfg(feature = "try-runtime")]
-use sp_std::vec::Vec;
-
-/// Collection of storage item formats from the previous storage version.
-mod old {
-	use frame_support::{pallet_prelude::OptionQuery, Blake2_128Concat};
-	use sp_core::U256;
-
-	use super::*;
-
-	/// V0 type for [`crate::Value`].
-	#[storage_alias]
-	pub(super) type AssetOwner<T: Config> = StorageMap<
-		Pallet<T>,
-		Blake2_128Concat,
-		U256,
-		<T as frame_system::Config>::AccountId,
-		OptionQuery,
-	>;
-}
+/// The log target of this pallet.
+pub const LOG_TARGET: &str = "runtime::living_assets_ownership";
 
 /// Unchecked version migration logic.
+// TODO: we should use `frame_support::migrations::VersionedMigration` when the following PR is
+// included in release. https://github.com/paritytech/polkadot-sdk/pull/1503
 pub mod version_unchecked {
-	use frame_support::{weights::Weight, StoragePrefixedMap};
-	use sp_io::{storage::clear_prefix, KillStorageResult};
+	use frame_support::{storage::migration::clear_storage_prefix, weights::Weight};
 
 	use super::*;
 
@@ -36,93 +19,54 @@ pub mod version_unchecked {
 	pub struct MigrateV0ToV1<T>(sp_std::marker::PhantomData<T>);
 
 	impl<T: Config> OnRuntimeUpgrade for MigrateV0ToV1<T> {
-		/// Return the existing `AssetOwner` storage item.
-		#[cfg(feature = "try-runtime")]
-		fn pre_upgrade() -> Result<Vec<u8>, sp_runtime::TryRuntimeError> {
-			use parity_scale_codec::Encode;
-
-			// access the storage item
-			let old_keys = old::AssetOwner::<T>::iter_keys().collect::<Vec<_>>();
-
-			// frame_support::log::debug!("old_keys: {:?}", old_keys);
-			log::info!("old_keys: {:?}", old_keys);
-
-			// encode the storage item
-			Ok(old_keys.encode())
-		}
-
-		/// Migrate the [`old::AssetOwner`] storage item to the new format [`crate::AssetOwner`]
+		/// Clear LivingAssetsOwnership pallet storage.
 		///
 		/// Since we lost the `collection_id` information, we can't migrate the storage items.
 		/// So we just purge all storage items.
-		///
-		/// This function is called during the runtime upgrade process.
-		fn on_runtime_upgrade() -> frame_support::weights::Weight {
-			log::info!(
-				target: "aaaaaaaaaaaa",
-				"Migration did not execute. This probably should be removed"
-			);
-			let db_weight = <T as frame_system::Config>::DbWeight::get();
+		fn on_runtime_upgrade() -> Weight {
+			log::info!(target: LOG_TARGET, "Running MigrateV0ToV1");
 
-			// simply purge all storage items
-			let mut weight_consumed = Weight::default();
+			if Pallet::<T>::on_chain_storage_version() < 1 {
+				assert_eq!(<Pallet<T>>::name(), "LivingAssetsOwnership");
+				let res = clear_storage_prefix(
+					<Pallet<T>>::name().as_bytes(),
+					b"AssetOwner",
+					b"",
+					None,
+					None,
+				);
 
-			// TODO: no limit is dangerous
-			match clear_prefix(&old::AssetOwner::<T>::final_prefix(), None) {
-				KillStorageResult::AllRemoved(all) => {
-					weight_consumed += db_weight.writes(all.into());
-				},
-				KillStorageResult::SomeRemaining(some) => {
-					weight_consumed += db_weight.writes(some.into());
-				},
+				if res.unique == 0 {
+					log::error!(
+						target: LOG_TARGET,
+						"No storage entries are cleared from 'LivingAssetsOwnership_AssetOwner' storage prefix.",
+					);
+				}
+
+				log::info!(
+					target: LOG_TARGET,
+					"Cleared '{}' entries from 'LivingAssetsOwnership_AssetOwner' storage prefix",
+					res.unique
+				);
+
+				if res.maybe_cursor.is_some() {
+					log::error!(
+						target: LOG_TARGET,
+						"Storage prefix 'LivingAssetsOwnership_AssetOwner' is not completely cleared."
+					);
+				}
+
+				// Update storage version.
+				StorageVersion::new(1).put::<Pallet<T>>();
+
+				T::DbWeight::get().writes(res.unique.into())
+			} else {
+				log::debug!(
+					target: LOG_TARGET,
+					"Migration v0_to_v1 is skipped"
+				);
+				Weight::zero()
 			}
-
-			frame_support::log::debug!("weight_consumed: {:?}", weight_consumed);
-
-			weight_consumed
-		}
-
-		/// Post-upgrade migration step.
-		#[cfg(feature = "try-runtime")]
-		fn post_upgrade(state: Vec<u8>) -> Result<(), sp_runtime::TryRuntimeError> {
-			use frame_support::ensure;
-			use parity_scale_codec::Decode;
-
-			let old_keys = Vec::<sp_core::U256>::decode(&mut &state[..])
-				.map_err(|_| sp_runtime::TryRuntimeError::Other("Failed to decode old_keys"))?;
-
-			frame_support::log::debug!("old_keys: {:?}", old_keys);
-
-			// check the old storage items
-			for old_key in old_keys {
-				let old_asset_owner = old::AssetOwner::<T>::get(old_key);
-				ensure!(old_asset_owner.is_none(), "WARNING: old_asset_owner is not None");
-			}
-
-			Ok(())
 		}
 	}
 }
-
-// Public module containing *version checked* migration logic.
-//
-// This is the only module that should be exported from this module.
-//
-// TODO: we should use `frame_support::migrations::VersionedMigration` when the following PR is
-// included in release. https://github.com/paritytech/polkadot-sdk/pull/1503
-// pub mod versioned {
-// 	use super::*;
-
-// 	/// `version_unchecked::MigrateV0ToV1` wrapped in a
-// 	/// [`VersionedMigration`](frame_support::migrations::VersionedMigration), which ensures that:
-// 	/// - The migration only runs once when the on-chain storage version is 0
-// 	/// - The on-chain storage version is updated to `1` after the migration executes
-// 	/// - Reads/Writes from checking/settings the on-chain storage version are accounted for
-// 	pub type MigrateV0ToV1<T> = frame_support::migrations::VersionedRuntimeUpgrade<
-// 		0, // The migration will only execute when the on-chain storage version is 0
-// 		1, // The on-chain storage version will be set to 1 after the migration is complete
-// 		version_unchecked::MigrateV0ToV1<T>,
-// 		crate::pallet::Pallet<T>,
-// 		<T as frame_system::Config>::DbWeight,
-// 	>;
-// }
