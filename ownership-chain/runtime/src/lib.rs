64646af19642e691984a397569220936660f8094
magecnion <magecnion@gmail.com>
2023-10-09 11:12:31 +0200

    revert

--- ownership-chain/runtime/src/lib.rs
+++ ownership-chain/runtime/src/lib.rs
@@ -156,7 +156,7 @@ pub type Executive = frame_executive::Executive<
 	frame_system::ChainContext<Runtime>,
 	Runtime,
 	AllPalletsWithSystem,
-	pallet_collator_selection::migration::v1::MigrateToV1<Runtime>,
+	Migrations,
 >;
 
 pub type Precompiles = FrontierPrecompiles<Runtime>;
