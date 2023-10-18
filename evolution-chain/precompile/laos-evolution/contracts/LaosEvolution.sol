003619c7e181beca5d60bf0365dba05d20712997
Dastan <dastan@freeverse.io>
2023-10-18 14:07:13 +0300

    Fix contract comments

--- evolution-chain/precompile/laos-evolution/contracts/LaosEvolution.sol
+++ evolution-chain/precompile/laos-evolution/contracts/LaosEvolution.sol
@@ -7,12 +7,13 @@ pragma solidity >=0.8.3;
 /// @custom:address 0x0000000000000000000000000000000000000403
 interface LivingAssets {
     /// @notice Event emitted when a new collection is created
-    /// @param collectionId Id of the newly created collection
-    /// @param owner Owner of the newly created collection
+    /// @param collectionId the id of the newly created collection
+    /// @param owner the owner of the newly created collection
     event NewCollection(uint64 collectionId, address indexed owner);
 
     /// @notice Creates a new collection
     /// @dev Call this function to create a new collection
-    /// @return collectionId id of the newly created collection
+    /// @param owner the owner of the newly created collection
+    /// @return collectionId the id of the newly created collection
     function createCollection(address owner) external returns (uint64);
 }
