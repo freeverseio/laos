6dd6df9bfb6f2ae674b76c3e03c795422f431c6e
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2023-10-20 10:56:09 +0200

    laos-evolution pallet trait has tokenURI

--- evolution-chain/pallets/laos-evolution/src/tests.rs
+++ evolution-chain/pallets/laos-evolution/src/tests.rs
@@ -9,7 +9,7 @@ use crate::{
 };
 use frame_support::{assert_noop, assert_ok};
 use parity_scale_codec::Encode;
-use sp_core::H160;
+use sp_core::{H160, U256};
 
 const ALICE: &str = "0x0000000000000000000000000000000000000005";
 const BOB: &str = "0x0000000000000000000000000000000000000006";
@@ -271,4 +271,21 @@ fn collection_owner_works() {
 			Some(AccountId::from_str(ALICE).unwrap())
 		);
 	})
+}
+
+#[test]
+fn token_uri_of_unexistent_collection_returns_none() {
+	new_test_ext().execute_with(|| {
+		let tocken_id: U256 = 0_u64.into();
+		assert_eq!(LaosEvolution::token_uri(0_u64, tocken_id), None);
+	});
+}
+
+#[test]
+fn token_uri_of_unexistent_token_returns_none() {
+	new_test_ext().execute_with(|| {
+		let collection_id = create_collection(ALICE);
+		let tocken_id: U256 = 0_u64.into();
+		assert_eq!(LaosEvolution::token_uri(collection_id, tocken_id), None);
+	});
 }
