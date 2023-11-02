daec8bb5f79d691f5dfc8ab3e6d3a21e8bbaa449
magecnion <magecnion@gmail.com>
2023-11-02 12:44:48 +0100

    fix tests

--- ownership-chain/e2e-tests/tests/test-create-collection.ts
+++ ownership-chain/e2e-tests/tests/test-create-collection.ts
@@ -4,7 +4,6 @@ import { GENESIS_ACCOUNT, GENESIS_ACCOUNT_BALANCE, GENESIS_ACCOUNT_PRIVATE_KEY }
 import LaosEvolution from "../build/contracts/LaosEvolution.json";
 import { AbiItem } from "web3-utils";
 import { expect } from "chai";
-import { ethers } from "ethers";
 
 describeWithExistingNode("Frontier RPC (Create Collection)", (context) => {
     const LAOS_EVOLUTION_ABI = LaosEvolution.abi as AbiItem[]
@@ -20,6 +19,7 @@ describeWithExistingNode("Frontier RPC (Create Collection)", (context) => {
     });
 
     step("when collection does not exist owner of call should fail", async function () {
+        const collectionId = "0";
         try {
             await contract.methods.ownerOfCollection(collectionId).call();
         } catch (error) {
@@ -30,8 +30,9 @@ describeWithExistingNode("Frontier RPC (Create Collection)", (context) => {
     });
 
     step("when collection is created should return owner", async function () {
-        this.timeout(55000);
+        this.timeout(70000);
 
+        const collectionId = "0";
         let nonce = await context.web3.eth.getTransactionCount(GENESIS_ACCOUNT);
         
         await context.web3.eth.accounts.wallet.add(GENESIS_ACCOUNT_PRIVATE_KEY);
@@ -45,6 +46,7 @@ describeWithExistingNode("Frontier RPC (Create Collection)", (context) => {
     step("when collection is created event is emitted", async function () {
         this.timeout(70000);
         
+        const collectionId = "1";
         let nonce = await context.web3.eth.getTransactionCount(GENESIS_ACCOUNT);
         const SELECTOR_LOG_NEW_COLLECTION = "0x6eb24fd767a7bcfa417f3fe25a2cb245d2ae52293d3c4a8f8c6450a09795d289";
 
@@ -53,7 +55,7 @@ describeWithExistingNode("Frontier RPC (Create Collection)", (context) => {
         expect(result.status).to.be.eq(true);
 
         expect(Object.keys(result.events).length).to.be.eq(1);
-        expect(result.events.NewCollection.returnValues.collectionId).to.be.eq("0");
+        expect(result.events.NewCollection.returnValues.collectionId).to.be.eq(collectionId);
         expect(result.events.NewCollection.returnValues.owner).to.be.eq(GENESIS_ACCOUNT);
 
         // event topics
@@ -62,7 +64,7 @@ describeWithExistingNode("Frontier RPC (Create Collection)", (context) => {
         expect(result.events.NewCollection.raw.topics[1]).to.be.eq(context.web3.utils.padLeft(GENESIS_ACCOUNT.toLowerCase(), 64));
 
         // event data
-        expect(result.events.NewCollection.raw.data).to.be.eq("0x"+context.web3.utils.padLeft(collectionId, 64));
+        expect(result.events.NewCollection.raw.data).to.be.eq("0x" + context.web3.utils.padLeft(collectionId, 64));
     });
 
 });
