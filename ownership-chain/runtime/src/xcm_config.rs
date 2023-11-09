4e0741d845388e1414b790aed17719c2d5eb9eed
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2023-11-09 08:59:50 +0100

    fix compilation dep errors

--- ownership-chain/runtime/src/xcm_config.rs
+++ ownership-chain/runtime/src/xcm_config.rs
@@ -10,7 +10,7 @@ use frame_support::{
 };
 use frame_system::{EnsureRoot, RawOrigin as SystemRawOrigin};
 use pallet_xcm::XcmPassthrough;
-use polkadot_parachain::primitives::Sibling;
+use polkadot_parachain_primitives::primitives::Sibling;
 use sp_runtime::traits::TryConvert;
 use xcm::latest::prelude::*;
 use xcm_builder::{
