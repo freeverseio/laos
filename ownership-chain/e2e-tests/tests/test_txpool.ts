caae7c7b33aed235c9fa5bd6e5c36baf1238114f
Dastan <dastan@freeverse.io>
2023-11-13 12:00:38 +0200

    Review comments

--- ownership-chain/e2e-tests/tests/test_txpool.ts
+++ ownership-chain/e2e-tests/tests/test_txpool.ts
@@ -7,9 +7,9 @@ import { customRequest, describeWithExistingNode } from "./util";
 describeWithExistingNode("Frontier RPC (TxPoolApi)", (context) => {
 	const TEST_CONTRACT_BYTECODE = "0x608060405234801561";
 
-	var nonce = 0;
-	let pending_tx;
-	let future_tx;
+	let nonce;
+	let pendingTx;
+	let futureTx;
 	async function sendTransaction(context, nonce) {
 		const tx = await context.web3.eth.accounts.signTransaction(
 			{
@@ -26,35 +26,46 @@ describeWithExistingNode("Frontier RPC (TxPoolApi)", (context) => {
 		return tx;
 	}
 
+	before(async function () {
+		nonce = await context.web3.eth.getTransactionCount(OWNCHAIN_SUDO);
+	});
+
 	step("txpool_status should return correct result", async function () {
-		let txpoolStatus = await customRequest(context.web3, "txpool_status", []);
-		expect(txpoolStatus.result.pending).to.be.equal("0x0");
-		expect(txpoolStatus.result.queued).to.be.equal("0x0");
+		let txpoolStatusBefore = await customRequest(context.web3, "txpool_status", []);
 
-		pending_tx = await sendTransaction(context, nonce);
-		future_tx = await sendTransaction(context, nonce + 3);
-		txpoolStatus = await customRequest(context.web3, "txpool_status", []);
+		pendingTx = await sendTransaction(context, nonce);
+		futureTx = await sendTransaction(context, nonce + 1000);
+		let txpoolStatusAfter = await customRequest(context.web3, "txpool_status", []);
 
-		expect(txpoolStatus.result.pending).to.be.equal("0x1");
-		expect(txpoolStatus.result.queued).to.be.equal("0x1");
+		expect(parseInt(txpoolStatusAfter.result.pending, 16)).to.be.equal(
+			parseInt(txpoolStatusBefore.result.pending, 16) + 1
+		);
+		expect(parseInt(txpoolStatusAfter.result.queued, 16)).to.be.equal(
+			parseInt(txpoolStatusBefore.result.queued, 16) + 1
+		);
 	});
 
 	step("txpool_content should return correct result", async function () {
 		let txpoolContent = await customRequest(context.web3, "txpool_content", []);
-		let genesisAccount = OWNCHAIN_SUDO.toLowerCase();
 
-		expect(txpoolContent.result.queued[genesisAccount]["0x3"].nonce).to.be.equal("0x3");
-		expect(txpoolContent.result.queued[genesisAccount]["0x3"].hash).to.be.equal(future_tx.transactionHash);
+		let genesisAccount = OWNCHAIN_SUDO.toLowerCase();
+		let futureNonce = `0x${(nonce + 1000).toString(16)}`;
+
+		expect(txpoolContent.result.queued[genesisAccount][futureNonce].nonce).to.be.equal(futureNonce);
+		expect(txpoolContent.result.queued[genesisAccount][futureNonce].hash).to.be.equal(futureTx.transactionHash);
 	});
 
 	step("txpool_inspect should return correct result", async function () {
 		let txpoolInspect = await customRequest(context.web3, "txpool_inspect", []);
 		let genesisAccount = OWNCHAIN_SUDO.toLowerCase();
 
-		expect(txpoolInspect.result.pending[genesisAccount]["0x0"]).to.be.equal(
+		let currentNonce = `0x${nonce.toString(16)}`;
+		let futureNonce = `0x${(nonce + 1000).toString(16)}`;
+
+		expect(txpoolInspect.result.pending[genesisAccount][currentNonce]).to.be.equal(
 			"0x0000000000000000000000000000000000000000: 0 wei + 1048576 gas x 1000000000 wei"
 		);
-		expect(txpoolInspect.result.queued[genesisAccount]["0x3"]).to.be.equal(
+		expect(txpoolInspect.result.queued[genesisAccount][futureNonce]).to.be.equal(
 			"0x0000000000000000000000000000000000000000: 0 wei + 1048576 gas x 1000000000 wei"
 		);
 	});
