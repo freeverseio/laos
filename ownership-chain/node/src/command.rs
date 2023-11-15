055e1e6023070c325dbfd3af969a5f8b8e50a0dd
Dastan <dastan@freeverse.io>
2023-11-15 19:24:03 +0200

    Don't rewrite `local-v`

--- ownership-chain/node/src/command.rs
+++ ownership-chain/node/src/command.rs
@@ -31,7 +31,7 @@ fn load_spec(id: &str) -> std::result::Result<Box<dyn ChainSpec>, String> {
 	Ok(match id {
 		"dev" => Box::new(chain_spec::development_config()),
 		"template-rococo" => Box::new(chain_spec::local_testnet_config()),
-		"" | "local" => Box::new(chain_spec::local_testnet_config()),
+		"" | "local" | "local-v" => Box::new(chain_spec::local_testnet_config()),
 		path => Box::new(chain_spec::ChainSpec::from_json_file(std::path::PathBuf::from(path))?),
 	})
 }
