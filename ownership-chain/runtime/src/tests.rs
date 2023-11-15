8a92e9c47a1a77db1e4af146b89767404c9c51d7
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2023-11-15 14:47:51 +0100

    Set Existential Deposit to Zero and Refactor Related Tests (#170)
    * create the test
    
    * existential deposit to 1
    
    * removed tests not necessary
    
    * refactoring
    
    * refactoring
    
    * using existential balance == 0
    
    * fmt
    
    * fix compilation error
    
    * add test for minimum amount
    
    * fmt
    
    * add ese test for minimum amount
    
    * sending 1 wei
    
    * Update ownership-chain/runtime/src/tests.rs
    
    Co-authored-by: dastan <137785454+dastanfv@users.noreply.github.com>
    
    * better commnet
    
    * fix the merge
    
    * better test name
    
    * comment identation
    
    * using assert! in test
    
    * protect the candidacy_bond from a 0 Esitential deposit
    
    * some consmetics
    
    * fmt
    
    * not needed to have a candidacy bond so high
    
    * move e2e test -> unit test
    
    * removed unuseful test
    
    ---------
    
    Co-authored-by: dastan <137785454+dastanfv@users.noreply.github.com>

--- ownership-chain/runtime/src/tests.rs
+++ ownership-chain/runtime/src/tests.rs
@@ -1,11 +1,29 @@
 use super::*;
-use frame_support::assert_ok;
+use fp_rpc::runtime_decl_for_ethereum_runtime_rpc_api::EthereumRuntimeRPCApiV5;
+use frame_support::{
+	assert_ok,
+	traits::tokens::{fungible::Balanced, Precision},
+};
 use sp_core::U256;
+use std::str::FromStr;
+
+const ALICE: &str = "0xf24FF3a9CF04c71Dbc94D0b566f7A27B94566cac";
+
+// Build genesis storage according to the mock runtime.
+pub fn new_test_ext() -> sp_io::TestExternalities {
+	frame_system::GenesisConfig::<Runtime>::default()
+		.build_storage()
+		.unwrap()
+		.into()
+}
+
+#[test]
+fn minimum_balance_should_be_0() {
+	assert_eq!(Balances::minimum_balance(), 0);
+}
 
 #[test]
 fn test_block_and_gas_limit_constants() {
-	use crate::Runtime;
-
 	let system_block_weights = <Runtime as frame_system::Config>::BlockWeights::get();
 
 	assert_ok!(system_block_weights.clone().validate());
@@ -26,4 +44,18 @@ fn test_multisig_constants() {
 	// 0.1 UNIT
 	assert_eq!(<Runtime as pallet_multisig::Config>::DepositFactor::get(), UNIT / 10);
 	assert_eq!(<Runtime as pallet_multisig::Config>::MaxSignatories::get(), 20);
+}
+
+#[test]
+fn send_1_minimum_unit_to_wallet_with_0_wei_balance_should_increase_balance_by_1_wei() {
+	new_test_ext().execute_with(|| {
+		let alice = AccountId::from_str(ALICE).unwrap();
+		assert_eq!(Runtime::account_basic(alice.into()).balance, 0.into());
+
+		let minimum_amount = 1;
+		assert!(Balances::deposit(&alice, minimum_amount, Precision::Exact).is_ok());
+		assert_eq!(Balances::total_balance(&alice), minimum_amount);
+
+		assert_eq!(Runtime::account_basic(alice.into()).balance, 1.into());
+	})
 }
