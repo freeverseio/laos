7d46425d5e2fd1bad8f3d95542926ff7ced1174e
magecnion <magecnion@gmail.com>
2024-01-08 12:30:15 +0100

    add tests

---
+++ ownership-chain/precompile/asset-metadata-extender/src/tests.rs
@@ -0,0 +1,92 @@
+#[test]
+fn check_log_selectors() {
+	todo!();
+}
+
+#[test]
+fn function_selectors() {
+	todo!();
+}
+
+#[test]
+fn call_unexistent_selector_should_fail() {
+	todo!();
+}
+
+mod create_metadata_extension {
+	#[test]
+	fn does_not_return_anything() {
+		todo!();
+	}
+
+	#[test]
+	fn reverts_when_claimer_already_has_metadata_extension_for_universal_location() {
+		todo!();
+	}
+
+	#[test]
+	fn on_mock_with_nonzero_value_fails() {
+		// duda onmock?
+		todo!();
+	}
+
+	#[test]
+	fn it_is_expected_to_have_a_cost() {
+		todo!();
+	}
+}
+
+mod balance_of_ul {
+	#[test]
+	fn given_unexistent_ul_returns_zero() {
+		todo!();
+	}
+	#[test]
+	fn returns_number_of_extensions() {
+		todo!();
+	}
+
+	#[test]
+	fn it_is_expected_to_have_a_cost() {
+		todo!();
+	}
+}
+
+mod claimer_of_ul_by_index {
+	#[test]
+	fn claimer_of_ul_by_index_given_unexistent_index_returns_empty_address() {
+		todo!();
+	}
+
+	#[test]
+	fn claimer_of_ul_by_index_returns_claimer() {
+		todo!();
+	}
+
+	#[test]
+	fn claimer_of_ul_by_index_it_is_expected_to_have_a_cost() {
+		todo!();
+	}
+}
+
+mod extension_of_ul_by_index {
+	#[test]
+	fn it_is_expected_to_have_a_cost() {
+		todo!();
+	}
+
+	#[test]
+	fn returns_extension() {
+		todo!();
+	}
+
+	#[test]
+	fn given_unexistent_ul_returns_empty_string() {
+		todo!();
+	}
+
+	#[test]
+	fn given_unexistent_index_returns_empty_string() {
+		todo!();
+	}
+}
