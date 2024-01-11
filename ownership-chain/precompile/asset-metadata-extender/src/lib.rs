883dae7710d473a7f0a20e384c1eff499cedc702
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2024-01-11 15:16:16 +0100

    refactoring

--- ownership-chain/precompile/asset-metadata-extender/src/lib.rs
+++ ownership-chain/precompile/asset-metadata-extender/src/lib.rs
@@ -1,8 +1,8 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 use fp_evm::{Precompile, PrecompileHandle, PrecompileOutput};
 use laos_precompile_utils::{
-	keccak256, revert_dispatch_error, succeed, Bytes, EvmDataReader, EvmResult, FunctionModifier,
-	PrecompileHandleExt, LogsBuilder, EvmDataWriter, LogExt,
+	keccak256, revert_dispatch_error, succeed, Bytes, EvmDataReader, EvmDataWriter, EvmResult,
+	FunctionModifier, LogExt, LogsBuilder, PrecompileHandleExt,
 };
 use pallet_asset_metadata_extender::{
 	traits::AssetMetadataExtender as AssetMetadataExtenderT,
@@ -100,43 +100,37 @@ where
 		let token_uri = Self::get_token_uri_from_input(input)?;
 		let claimer = context.caller;
 
-		match AssetMetadataExtender::<Runtime>::update_token_uri_extension(
-			claimer.clone().into(),
+		let result = AssetMetadataExtender::<Runtime>::update_token_uri_extension(
+			claimer.into(),
 			universal_location.into(),
 			token_uri.clone().into(),
-		) {
-			Ok(_) => { 
-			LogsBuilder::new(context.address)
-					.log2(
-						SELECTOR_LOG_EXTENDED_TOKEN_URI_UPDATED,
-						claimer,
-						EvmDataWriter::new()
-							.write(Bytes(token_uri.into()))
-							.build(),
-					)
-					.record(handle)?;
+		);
 
-				Ok(succeed(sp_std::vec![]))
-			},
-			Err(err) => Err(revert_dispatch_error(err)),
+		if let Err(err) = result {
+			return Err(revert_dispatch_error(err));
 		}
+
+		LogsBuilder::new(context.address)
+			.log2(
+				SELECTOR_LOG_EXTENDED_TOKEN_URI_UPDATED,
+				claimer,
+				EvmDataWriter::new().write(Bytes(token_uri.into())).build(),
+			)
+			.record(handle)?;
+
+		Ok(succeed(sp_std::vec![]))
 	}
 
-	fn get_ul_from_input(
-		mut input: EvmDataReader,
-	) -> EvmResult<UniversalLocationOf<Runtime>> {
+	fn get_ul_from_input(mut input: EvmDataReader) -> EvmResult<UniversalLocationOf<Runtime>> {
 		let universal_location = input.read::<Bytes>()?.0;
-		let universal_location  =
-			universal_location
-				.clone()
-				.try_into()
-				.map_err(|_| revert("invalid universal location length"))?;
+		let universal_location = universal_location
+			.clone()
+			.try_into()
+			.map_err(|_| revert("invalid universal location length"))?;
 		Ok(universal_location)
 	}
 
-	fn get_token_uri_from_input(
-		mut input: EvmDataReader,
-	) -> EvmResult<TokenUriOf<Runtime>> {
+	fn get_token_uri_from_input(mut input: EvmDataReader) -> EvmResult<TokenUriOf<Runtime>> {
 		let token_uri = input.read::<Bytes>()?.0;
 		let token_uri =
 			token_uri.clone().try_into().map_err(|_| revert("invalid token uri length"))?;
