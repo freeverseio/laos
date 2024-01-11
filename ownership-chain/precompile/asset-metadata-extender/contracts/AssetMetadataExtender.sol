aec24d1bd219c42bd4c3d83c9c08b124d0af333e
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2024-01-11 11:44:34 +0100

    added inteface for update

--- ownership-chain/precompile/asset-metadata-extender/contracts/AssetMetadataExtender.sol
+++ ownership-chain/precompile/asset-metadata-extender/contracts/AssetMetadataExtender.sol
@@ -14,4 +14,22 @@ interface AssetMetadataExtender {
         uint256 indexed _universalLocation,
         string _tokenURI
     );
+
+    /// @notice Emitted when an extended token's URI is updated
+    /// @param _claimer The address of the user who updated the token URI
+    /// @param _uloc The universal location identifier of the token as a string
+    /// @param _tokenURI The new token URI after the update
+    event ExtendedTokenURIUpdated(
+        address indexed _claimer,
+        string indexed _uloc,
+        string _tokenURI
+    );
+
+    /// @notice Updates the URI of an extended token
+    /// @param _uloc The universal location identifier of the token
+    /// @param _tokenURI The new URI to be set for the token
+    function updateExtendedTokenURI(
+        string calldata _uloc,
+        string calldata _tokenURI
+    ) external;
 }
