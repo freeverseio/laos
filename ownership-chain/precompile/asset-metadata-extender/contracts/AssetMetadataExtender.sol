10aa3304c80ffe0acbccebcf827a9c56e14b2ce3
Dastan <dastan@freeverse.io>
2024-01-15 16:28:12 +0100

    Add precompile

--- ownership-chain/precompile/asset-metadata-extender/contracts/AssetMetadataExtender.sol
+++ ownership-chain/precompile/asset-metadata-extender/contracts/AssetMetadataExtender.sol
@@ -4,6 +4,7 @@ pragma solidity >=0.8.3;
 /// @title Pallet Asset Metadata Extender Interface
 /// @author LAOS Team
 /// @notice This interface allows Solidity contracts to interact with pallet-asset-metadata-extender
+/// @custom:address 0x0000000000000000000000000000000000000404
 interface AssetMetadataExtender {
     /// @notice Emitted when a token metadata is extended
     /// @param _claimer the address of the caller
