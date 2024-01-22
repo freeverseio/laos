796f537efffcc6b870e575066318b49b3bfe3e28
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2024-01-22 11:51:45 +0100

    first test on ramping

--- ownership-chain/runtime/src/tests/constant_tests.rs
+++ ownership-chain/runtime/src/tests/constant_tests.rs
@@ -13,6 +13,7 @@ use frame_support::{
 use sp_core::U256;
 
 const ALICE: &str = "0xf24FF3a9CF04c71Dbc94D0b566f7A27B94566cac";
+const BOB: &str = "0x6c2b9c9b5007740e52d80dddb8e197b0c844f239";
 
 #[test]
 fn minimum_balance_should_be_0() {
@@ -66,3 +67,33 @@ fn check_pallet_vesting_configuration() {
 	);
 	assert_eq!(<Runtime as pallet_vesting::Config>::MAX_VESTING_SCHEDULES, 28);
 }
+
+#[test]
+fn account_should_have_a_clief_of_24_blocks_and_vests_over_24x4_blocks_after_cliff() {
+	new_test_ext().execute_with(|| {
+		let alice = AccountId::from_str(ALICE).unwrap();
+		let bob = AccountId::from_str(BOB).unwrap();
+		let starting_block = 24_u32;
+		let vesting_period = 24 * 4;
+		let per_block = UNIT;
+		let total_amount = vesting_period * per_block;
+		let alice_origin = <Runtime as frame_system::Config>::RuntimeOrigin::signed(alice);
+		let bob_origin = <Runtime as frame_system::Config>::RuntimeOrigin::signed(bob);
+
+		assert!(Balances::deposit(&alice, total_amount, Precision::Exact).is_ok());
+		assert_eq!(Balances::total_balance(&alice), total_amount);
+		assert_eq!(Balances::total_balance(&bob), 0);
+
+		let vesting_info = pallet_vesting::VestingInfo::new(total_amount, per_block, starting_block); 
+		assert!(vesting_info.clone().is_valid());
+
+		assert_ok!(pallet_vesting::Pallet::<Runtime>::vested_transfer(alice_origin, bob.clone(), vesting_info));
+
+		for i in 24..=starting_block + vesting_period as u32 {
+			frame_system::Pallet::<Runtime>::set_block_number(i);
+			assert_ok!(pallet_vesting::Pallet::<Runtime>::vest(bob_origin.clone()));
+			assert_eq!(Balances::usable_balance(&bob), (i - starting_block) as u128 * UNIT);
+			assert_eq!(Balances::total_balance(&bob), total_amount);
+		}
+	});
+} 
