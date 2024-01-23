3626df76a8d95388094656469895c5c090998097
magecnion <magecnion@gmail.com>
2024-01-23 11:51:08 +0100

    Update runtime and config versions

--- ownership-chain/e2e-tests/tests/config.ts
+++ ownership-chain/e2e-tests/tests/config.ts
@@ -6,7 +6,7 @@ import AssetMetadataExtender from "../build/contracts/AssetMetadataExtender.json
 
 // Node config
 export const RUNTIME_SPEC_NAME = "frontier-template";
-export const RUNTIME_SPEC_VERSION = 1001;
+export const RUNTIME_SPEC_VERSION = 1010;
 export const RUNTIME_IMPL_VERSION = 0;
 export const RPC_PORT = 9999;
 
