0ba3dd2ef65485af7392c5a8f308a204fe6e0da6
magecnion <magecnion@gmail.com>
2024-01-24 16:41:42 +0100

    Update package versions and runtime spec version

--- ownership-chain/runtime/src/lib.rs
+++ ownership-chain/runtime/src/lib.rs
@@ -206,7 +206,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("laos"),
 	impl_name: create_runtime_str!("freeverse-laos"),
 	authoring_version: 1,
-	spec_version: 1100,
+	spec_version: 1001,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,
