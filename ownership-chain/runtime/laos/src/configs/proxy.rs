19b33f4604b70f9bf245edaa6c9e7e4b38c702c1
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2024-02-27 17:44:07 +0100

    added comments

--- ownership-chain/runtime/laos/src/configs/proxy.rs
+++ ownership-chain/runtime/laos/src/configs/proxy.rs
@@ -15,11 +15,17 @@ const fn calculate_deposit(items: u32, bytes: u32) -> Balance {
 }
 
 parameter_types! {
+	// One storage item; key size 32, value size 8
 	pub const ProxyDepositBase: Balance = calculate_deposit(1, 8);
+	// Additional storage item size of 21 bytes (20 bytes AccountId + 1 byte sizeof(ProxyType)).
 	pub const ProxyDepositFactor: Balance = calculate_deposit(0, 21);
 	pub const MaxProxies: u16 = 32;
 	pub const MaxPending: u16 = 32;
 	pub const AnnouncementDepositBase: Balance = calculate_deposit(1, 8);
+	// Additional storage item size of 56 bytes:
+	// - 20 bytes AccountId
+	// - 32 bytes Hasher (Blake2256)
+	// - 4 bytes BlockNumber (u32)
 	pub const AnnouncementDepositFactor: Balance = calculate_deposit(0, 56);
 }
 
@@ -53,7 +59,7 @@ impl pallet_proxy::Config for Runtime {
 )]
 pub enum ProxyType {
 	/// Represents a proxy type that allows any call to be proxied.
-	Any,
+	Any = 0,
 }
 
 impl Default for ProxyType {
