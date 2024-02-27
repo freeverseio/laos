78583d12a89a4ea3cb1ecc6666edd3a2ce6fbdad
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2024-02-27 08:00:53 +0100

    removed comments

--- ownership-chain/runtime/laos/src/lib.rs
+++ ownership-chain/runtime/laos/src/lib.rs
@@ -784,24 +784,11 @@ impl InstanceFilter<RuntimeCall> for ProxyType {
 }
 
 parameter_types! {
-	// One storage item; key size 32, value size 8; .
-	// pub const ProxyDepositBase: Balance = deposit(1, 8);
-	// Additional storage item size of 33 bytes.
-	// pub const ProxyDepositFactor: Balance = deposit(0, 33);
-//	pub const MaxProxies: u16 = 32;
-//	pub const AnnouncementDepositBase: Balance = deposit(1, 8);
-//	pub const AnnouncementDepositFactor: Balance = deposit(0, 66);
-//	pub const MaxPending: u16 = 32;
 	pub const ProxyDepositBase: Balance = deposit(1, 8);
-	// Additional storage item size of 21 bytes (20 bytes AccountId + 1 byte sizeof(ProxyType)).
 	pub const ProxyDepositFactor: Balance = deposit(0, 21);
 	pub const MaxProxies: u16 = 32;
 	pub const MaxPending: u16 = 32;
 	pub const AnnouncementDepositBase: Balance = deposit(1, 8);
-	// Additional storage item size of 56 bytes:
-	// - 20 bytes AccountId
-	// - 32 bytes Hasher (Blake2256)
-	// - 4 bytes BlockNumber (u32)
 	pub const AnnouncementDepositFactor: Balance = deposit(0, 56);
 }
 
