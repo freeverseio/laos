338aa2772589ce76ca19bf8656f67dbbe8ceeda6
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2024-03-01 16:29:36 +0100

    some fees go to RewardAccount

--- ownership-chain/runtime/laos/src/fee.rs
+++ ownership-chain/runtime/laos/src/fee.rs
@@ -1,25 +1,31 @@
-use super::PhantomData;
-use frame_support::traits::{fungible::Credit, OnUnbalanced};
+use crate::{AccountId, PhantomData, Runtime};
+use frame_support::traits::{
+	fungible::{Balanced, Credit},
+	OnUnbalanced,
+};
 
 /// This is a dummy implementation for `OnUnbalanced` trait.
 pub struct DealWithFees<R>(PhantomData<R>);
 
-type NegativeImbalanceOfBalances<T> = pallet_balances::NegativeImbalance<T>;
-impl<R> OnUnbalanced<NegativeImbalanceOfBalances<R>> for DealWithFees<R>
+impl<R> OnUnbalanced<pallet_balances::NegativeImbalance<R>> for DealWithFees<R>
 where
 	R: pallet_balances::Config + pallet_parachain_staking::Config,
 {
-	fn on_nonzero_unbalanced(_amount: NegativeImbalanceOfBalances<R>) {
-		// TODO actually the fees are burned
+	fn on_nonzero_unbalanced(amount: pallet_balances::NegativeImbalance<R>) {
+		// <ToRewardsAccount<R> as OnUnbalanced<_>>::on_unbalanced(amount);
 	}
 }
 
 impl<R> OnUnbalanced<Credit<R::AccountId, pallet_balances::Pallet<R>>> for DealWithFees<R>
 where
-	R: pallet_balances::Config,
+	R: pallet_balances::Config + pallet_block_rewards_source::Config,
 {
 	// this is called from pallet_evm for Ethereum-based transactions
-	fn on_nonzero_unbalanced(_amount: Credit<R::AccountId, pallet_balances::Pallet<R>>) {
-		// TODO actually the fees are burned
+	fn on_nonzero_unbalanced(amount: Credit<R::AccountId, pallet_balances::Pallet<R>>) {
+		if let Some(rewards_account) =
+			pallet_block_rewards_source::pallet::Pallet::<R>::rewards_account()
+		{
+			let _ = <pallet_balances::Pallet<R>>::resolve(&rewards_account, amount);
+		}
 	}
 }
