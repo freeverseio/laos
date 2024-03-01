61e4a0d0531e05db4844de0a32e404d5bcfdff6b
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2024-03-01 10:51:25 +0100

    minimum changes

--- ownership-chain/runtime/laos/src/configs/parachain_staking.rs
+++ ownership-chain/runtime/laos/src/configs/parachain_staking.rs
@@ -54,6 +54,7 @@ impl pallet_parachain_staking::Config for Runtime {
 	type MaxDelegationsPerDelegator = MaxDelegationsPerDelegator;
 	type MinCandidateStk = MinCandidateStk;
 	type MinDelegation = MinDelegation;
+	type BlockAuthor = ();
 	type OnCollatorPayout = ();
 	type PayoutCollatorReward = (); // TODO
 	type OnInactiveCollator = (); // TODO
