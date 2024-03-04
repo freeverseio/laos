cc29397bb1287d7f7c8e8801a1c782702c5d354a
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2024-03-04 15:53:08 +0100

    added TODO

--- ownership-chain/runtime/laos/src/configs/transaction_payment.rs
+++ ownership-chain/runtime/laos/src/configs/transaction_payment.rs
@@ -5,7 +5,7 @@ use polkadot_runtime_common::SlowAdjustingFeeUpdate;
 
 parameter_types! {
 	/// Relay Chain `TransactionByteFee` / 10
-	pub const TransactionByteFee: Balance = 10 * MICROUNIT;
+	pub const TransactionByteFee: Balance = 10 * MICROUNIT; // TODO check this value with the one used in deposit function
 	pub const OperationalFeeMultiplier: u8 = 5;
 }
 
