267226327d7fb1dbaad0fe89d5a8ce7e27e8ca9d
magecnion <magecnion@gmail.com>
2024-03-05 20:03:36 +0100

    Remove unused import in block_rewards_source.rs

--- ownership-chain/runtime/laos/src/configs/block_rewards_source.rs
+++ ownership-chain/runtime/laos/src/configs/block_rewards_source.rs
@@ -1,4 +1,4 @@
-use crate::{Runtime, RuntimeEvent};
+use crate::Runtime;
 
 impl pallet_block_rewards_source::Config for Runtime {
 	type WeightInfo = pallet_block_rewards_source::weights::SubstrateWeight<Runtime>;
