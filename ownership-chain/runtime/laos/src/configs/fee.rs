f98db7f10328b808954974f3dada6df5836f607f
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2024-03-05 16:17:06 +0100

    refactoring

--- ownership-chain/runtime/laos/src/configs/fee.rs
+++ ownership-chain/runtime/laos/src/configs/fee.rs
@@ -2,6 +2,24 @@ use crate::PhantomData;
 use frame_support::traits::{tokens::currency::Currency, Imbalance, OnUnbalanced};
 
 pub struct DealWithFees<R>(PhantomData<R>);
+
+impl<R> DealWithFees<R>
+where
+	R: pallet_balances::Config + pallet_block_rewards_source::Config + pallet_authorship::Config,
+{
+	fn to_rewards_account(imbalance: pallet_balances::NegativeImbalance<R>) {
+		if let Some(rewards_account) = <pallet_block_rewards_source::Pallet<R>>::rewards_account() {
+			<pallet_balances::Pallet<R>>::resolve_creating(&rewards_account, imbalance);
+		}
+	}
+
+	fn to_author(imbalance: pallet_balances::NegativeImbalance<R>) {
+		if let Some(author) = <pallet_authorship::Pallet<R>>::author() {
+			<pallet_balances::Pallet<R>>::resolve_creating(&author, imbalance);
+		}
+	}
+}
+
 impl<R> OnUnbalanced<pallet_balances::NegativeImbalance<R>> for DealWithFees<R>
 where
 	R: pallet_balances::Config + pallet_block_rewards_source::Config + pallet_authorship::Config,
@@ -12,25 +30,15 @@ where
 	) {
 		if let Some(fees) = fees_then_tips.next() {
 			// 80% to rewards account, 20% to author
-			let (to_rewards_account, mut to_author) = fees.ration(80, 20);
+			let (rewards_account_amount, mut author_amount) = fees.ration(80, 20);
 
 			if let Some(tips) = fees_then_tips.next() {
 				// for tips, if any, 100% to author
-				tips.merge_into(&mut to_author);
+				tips.merge_into(&mut author_amount);
 			}
 
-			if let Some(rewards_account) =
-				<pallet_block_rewards_source::Pallet<R>>::rewards_account()
-			{
-				<pallet_balances::Pallet<R>>::resolve_creating(
-					&rewards_account,
-					to_rewards_account,
-				);
-			};
-
-			if let Some(author) = <pallet_authorship::Pallet<R>>::author() {
-				<pallet_balances::Pallet<R>>::resolve_creating(&author, to_author);
-			};
+			Self::to_rewards_account(rewards_account_amount);
+			Self::to_author(author_amount);
 		}
 	}
 
@@ -38,14 +46,9 @@ where
 	// (technically, it calls on_unbalanced, which calls this when non-zero)
 	fn on_nonzero_unbalanced(amount: pallet_balances::NegativeImbalance<R>) {
 		// 80% to rewards account, 20% to author
-		let (to_rewards_account, to_author) = amount.ration(80, 20);
+		let (rewards_account_amount, author_amount) = amount.ration(80, 20);
 
-		if let Some(rewards_account) = <pallet_block_rewards_source::Pallet<R>>::rewards_account() {
-			<pallet_balances::Pallet<R>>::resolve_creating(&rewards_account, to_rewards_account);
-		};
-
-		if let Some(author) = <pallet_authorship::Pallet<R>>::author() {
-			<pallet_balances::Pallet<R>>::resolve_creating(&author, to_author);
-		};
+		Self::to_rewards_account(rewards_account_amount);
+		Self::to_author(author_amount);
 	}
 }
