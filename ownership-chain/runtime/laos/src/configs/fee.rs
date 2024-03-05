708ee79f137da3c46d30ac871735a402f9253bbb
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2024-03-05 16:03:57 +0100

    compiling

--- ownership-chain/runtime/laos/src/configs/fee.rs
+++ ownership-chain/runtime/laos/src/configs/fee.rs
@@ -1,80 +1,45 @@
 use crate::PhantomData;
-use frame_support::traits::{
-	fungible::{Balanced, Credit},
-	OnUnbalanced,
-};
-
-// pub struct DealWithFees<R>(PhantomData<R>);
-
-// impl<R> OnUnbalanced<pallet_balances::NegativeImbalance<R>> for DealWithFees<R>
-// where
-// 	R: pallet_balances::Config + pallet_parachain_staking::Config,
-// {
-// 	fn on_nonzero_unbalanced(_amount: pallet_balances::NegativeImbalance<R>) {}
-// }
-
-// impl<R> OnUnbalanced<Credit<R::AccountId, pallet_balances::Pallet<R>>> for DealWithFees<R>
-// where
-// 	R: pallet_balances::Config + pallet_block_rewards_source::Config,
-// {
-// 	// this is called from pallet_evm for Ethereum-based transactions
-// 	fn on_nonzero_unbalanced(amount: Credit<R::AccountId, pallet_balances::Pallet<R>>) {
-// 		if let Some(rewards_account) =
-// 			pallet_block_rewards_source::pallet::Pallet::<R>::rewards_account()
-// 		{
-// 			let _ = <pallet_balances::Pallet<R>>::resolve(&rewards_account, amount);
-// 		}
-// 	}
-// }
+use frame_support::traits::{tokens::currency::Currency, Imbalance, OnUnbalanced};
 
 pub struct DealWithFees<R>(PhantomData<R>);
 impl<R> OnUnbalanced<pallet_balances::NegativeImbalance<R>> for DealWithFees<R>
 where
-	R: pallet_balances::Config + pallet_block_rewards_source::Config,
+	R: pallet_balances::Config + pallet_block_rewards_source::Config + pallet_authorship::Config,
 {
 	// this seems to be called for substrate-based transactions
 	fn on_unbalanceds<B>(
 		mut fees_then_tips: impl Iterator<Item = pallet_balances::NegativeImbalance<R>>,
 	) {
 		if let Some(fees) = fees_then_tips.next() {
+			// 80% to rewards account, 20% to author
+			let (to_rewards_account, mut to_author) = fees.ration(80, 20);
+
 			if let Some(tips) = fees_then_tips.next() {
+				// for tips, if any, 100% to author
+				tips.merge_into(&mut to_author);
 			}
-			// <ToStakingPot<R> as OnUnbalanced<_>>::on_unbalanced(fees);
-			// <ToAuthor<R> as OnUnbalanced<_>>::on_unbalanced(split.1);
-let staking_pot = <pallet_block_rewards_source::Pallet<R>>::rewards_account().unwrap(); // TODO
-		let _ = <pallet_balances::Pallet<R>>::resolve(&staking_pot, fees);
+
+			let rewards_account =
+				<pallet_block_rewards_source::Pallet<R>>::rewards_account().unwrap(); // TODO
+			<pallet_balances::Pallet<R>>::resolve_creating(&rewards_account, to_rewards_account);
+
+			if let Some(author) = <pallet_authorship::Pallet<R>>::author() {
+				<pallet_balances::Pallet<R>>::resolve_creating(&author, to_author);
+			};
 		}
 	}
 
 	// this is called from pallet_evm for Ethereum-based transactions
 	// (technically, it calls on_unbalanced, which calls this when non-zero)
-	fn on_nonzero_unbalanced(amount: pallet_balances::NegativeImbalance<R>) {}
+	fn on_nonzero_unbalanced(amount: pallet_balances::NegativeImbalance<R>) {
+		// 80% to rewards account, 20% to author
+		let (to_rewards_account, to_author) = amount.ration(80, 20);
+
+		let rewards_account = <pallet_block_rewards_source::Pallet<R>>::rewards_account().unwrap(); // TODO
+		<pallet_balances::Pallet<R>>::resolve_creating(&rewards_account, to_rewards_account);
+
+		if let Some(author) = <pallet_authorship::Pallet<R>>::author() {
+			<pallet_balances::Pallet<R>>::resolve_creating(&author, to_author);
+		};
+	}
 }
-
-
-// Logic for the author to get a portion of fees.
-// pub struct ToAuthor<R>(sp_std::marker::PhantomData<R>);
-// impl<R> OnUnbalanced<Credit<R::AccountId, pallet_balances::Pallet<R>>> for ToAuthor<R>
-// where
-// 	R: pallet_balances::Config + pallet_authorship::Config,
-// 	<R as frame_system::Config>::AccountId: From<AccountId>,
-// 	<R as frame_system::Config>::AccountId: Into<AccountId>,
-// {
-// 	fn on_nonzero_unbalanced(amount: Credit<<R as frame_system::Config>::AccountId, pallet_balances::Pallet<R>>) {
-// 		if let Some(author) = <pallet_authorship::Pallet<R>>::author() {
-// 			let _ = <pallet_balances::Pallet<R>>::resolve(&author, amount);
-// 		}
-// 	}
-// }
-
-// /// Implementation of `OnUnbalanced` that deposits the fees into  the "Blockchain Operation Treasury" for later payout.
-// pub struct ToStakingPot<R>(PhantomData<R>);
-// impl<R> OnUnbalanced<Credit<R::AccountId, pallet_balances::Pallet<R>>> for ToStakingPot<R>
-// where
-// 	R: pallet_balances::Config + pallet_block_rewards_source::Config ,
-// {
-// 	fn on_nonzero_unbalanced(amount: Credit<<R as frame_system::Config>::AccountId, pallet_balances::Pallet<R>>) {
-// 		let staking_pot = <pallet_block_rewards_source::Pallet<R>>::rewards_account().unwrap(); // TODO
-// 		let _ = <pallet_balances::Pallet<R>>::resolve(&staking_pot, amount);
-// 	}
-// }
