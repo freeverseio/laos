77881cf8b6ea50c247842b026117b89e98dca1a7
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2024-03-05 16:44:19 +0100

    added TODO

--- ownership-chain/runtime/laos/src/configs/transaction_payment.rs
+++ ownership-chain/runtime/laos/src/configs/transaction_payment.rs
@@ -1,7 +1,8 @@
 use super::fee::DealWithFees;
 use crate::{Balance, Balances, Runtime, RuntimeEvent, WeightToFee, MICROUNIT};
-use frame_support::{parameter_types, weights::ConstantMultiplier};
+use frame_support::{parameter_types, weights::{ConstantMultiplier, WeightToFeeCoefficients, WeightToFeePolynomial}};
 use polkadot_runtime_common::SlowAdjustingFeeUpdate;
+use smallvec::smallvec;
 
 parameter_types! {
 	/// Relay Chain `TransactionByteFee` / 10
@@ -9,12 +10,13 @@ parameter_types! {
 	pub const OperationalFeeMultiplier: u8 = 5;
 }
 
+// TODO: following has to be checked with this: https://github.com/moonbeam-foundation/moonbeam/blob/31dd0f3703d844b139a2f0fafde91025a8b97771/runtime/moonbeam/src/lib.rs#L361
 impl pallet_transaction_payment::Config for Runtime {
 	type RuntimeEvent = RuntimeEvent;
 	type OnChargeTransaction =
 		pallet_transaction_payment::CurrencyAdapter<Balances, DealWithFees<Runtime>>;
+	type OperationalFeeMultiplier = OperationalFeeMultiplier;
 	type WeightToFee = WeightToFee;
 	type LengthToFee = ConstantMultiplier<Balance, TransactionByteFee>;
 	type FeeMultiplierUpdate = SlowAdjustingFeeUpdate<Self>;
-	type OperationalFeeMultiplier = OperationalFeeMultiplier;
 }
