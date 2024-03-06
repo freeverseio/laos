781cb7c2b98fe001210dabcf598b0765ed817387
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2024-03-06 17:08:26 +0100

    test completed

--- ownership-chain/runtime/laos/src/configs/fee.rs
+++ ownership-chain/runtime/laos/src/configs/fee.rs
@@ -20,7 +20,7 @@ mod tests {
 	use crate::{generic::DigestItem, H256, BlakeTwo256, IdentityLookup};
 	use frame_support::{
 		parameter_types,
-		traits::{ConstU16, ConstU64, FindAuthor},
+		traits::{ConstU16, ConstU64, tokens::Precision, fungible::Balanced},
 		ConsensusEngineId,
 	};
 	use sp_runtime::{
@@ -184,33 +184,34 @@ mod tests {
 		});
 	}
 
-	// #[test]
-	// fn with_no_author_fee_should_be_burned() {
-	// 	new_test_ext().execute_with(|| {
-	// 		let fee_amount = 100;
+	#[test]
+	fn with_no_author_fee_should_be_burned() {
+		new_test_ext().execute_with(|| {
+			let fee_amount = 100;
 
-	// 		assert_eq!(
-	// 			pallet_authorship::Pallet::<Test>::author(),
-	// 			None,
-	// 			"Author should not be set"
-	// 		);
+			assert_eq!(
+				pallet_authorship::Pallet::<Test>::author(),
+				None,
+				"Author should not be set"
+			);
 
+			assert!(pallet_balances::Pallet::<Test>::deposit(&66, fee_amount * 2, Precision::Exact).is_ok());
 
-	// 		let initial_total_issuance = pallet_balances::Pallet::<Test>::total_issuance();
+			let initial_total_issuance = pallet_balances::Pallet::<Test>::total_issuance();
 
-	// 		// Mock the creation of a negative imbalance of 100 units
-	// 		let imbalance = pallet_balances::NegativeImbalance::new(fee_amount);
+			// Mock the creation of a negative imbalance of 100 units
+			let imbalance = pallet_balances::NegativeImbalance::new(fee_amount);
 
-	// 		// Distribute the fees
-	// 		ToAuthor::<Test>::on_unbalanceds(vec![imbalance].into_iter());
+			// Distribute the fees
+			ToAuthor::<Test>::on_unbalanceds(vec![imbalance].into_iter());
 
-	// 		let total_issuance = pallet_balances::Pallet::<Test>::total_issuance();
-	// 		let expected_issuance = initial_total_issuance - fee_amount;
+			let total_issuance = pallet_balances::Pallet::<Test>::total_issuance();
+			let expected_issuance = initial_total_issuance - fee_amount;
 
-	// 		assert_eq!(
-	// 			total_issuance, expected_issuance,
-	// 			"Total issuance did not decrease by the correct amount"
-	// 		);
-	// 	});
-	// }
+			assert_eq!(
+				total_issuance, expected_issuance,
+				"Total issuance did not decrease by the correct amount"
+			);
+		});
+	}
 }
