1a8c787922fed8927c9e7492b43d72525cb0b941
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2024-03-15 12:28:27 +0100

    refactoring

--- pallets/parachain-staking/src/rewards/mod.rs
+++ pallets/parachain-staking/src/rewards/mod.rs
@@ -1,6 +1,7 @@
 mod minting_rewards;
 mod transfer_from_rewards_account;
 
+pub use minting_rewards::MintingRewards;
 pub use transfer_from_rewards_account::TransferFromRewardsAccount;
 
 use crate::{
