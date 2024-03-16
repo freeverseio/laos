72acbac35c9b63b6848e0fb6188a4a71b427b77d
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2024-03-16 07:29:36 +0100

    extract benchmarks

--- runtime/laos/src/lib.rs
+++ runtime/laos/src/lib.rs
@@ -283,20 +283,9 @@ impl fp_self_contained::SelfContainedCall for RuntimeCall {
 	}
 }
 
-#[cfg(feature = "runtime-benchmarks")]
-mod benches {
-	frame_benchmarking::define_benchmarks!(
-		[frame_system, SystemBench::<Runtime>]
-		[pallet_balances, Balances]
-		[pallet_session, SessionBench::<Runtime>]
-		[pallet_timestamp, Timestamp]
-		[cumulus_pallet_xcmp_queue, XcmpQueue]
-		[pallet_laos_evolution, LaosEvolution]
-		[pallet_asset_metadata_extender, AssetMetadataExtender]
-		[pallet_parachain_staking, ParachainStaking]
-		[pallet_block_rewards_handler, BlockRewardsHandler]
-	);
-}
-
 #[cfg(test)]
 mod tests;
+
+#[cfg(feature = "runtime-benchmarks")]
+mod benchmarks;
+
