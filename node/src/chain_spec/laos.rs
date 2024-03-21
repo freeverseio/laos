077db64ffd812f9eb241a9ee59458ade1334546e
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2024-03-21 11:35:44 +0100

    removed balances of the EVM

--- node/src/chain_spec/laos.rs
+++ node/src/chain_spec/laos.rs
@@ -117,7 +117,7 @@ fn testnet_genesis(
 			..Default::default()
 		},
 		balances: laos_runtime::BalancesConfig {
-			balances: endowed_accounts.iter().cloned().map(|k| (k, 1e24 as u128)).collect(),
+			balances: vec![(predefined_accounts::ALITH.into(), 150000 * 1000000000000000000)],
 		},
 		parachain_info: laos_runtime::ParachainInfoConfig {
 			parachain_id: id,
@@ -170,78 +170,20 @@ fn testnet_genesis(
 			..Default::default()
 		},
 		evm: laos_runtime::EVMConfig {
-			accounts: {
-				let mut map: BTreeMap<_, _> = Precompiles::used_addresses()
-					.iter()
-					.map(|&address| {
-						(
-							address,
-							GenesisAccount {
-								nonce: Default::default(),
-								balance: Default::default(),
-								storage: Default::default(),
-								code: REVERT_BYTECODE.into(),
-							},
-						)
-					})
-					.collect();
-
-				map.insert(
-					// H160 address of Alice dev account
-					// Derived from SS58 (42 prefix) address
-					// SS58: 5GrwvaEF5zXb26Fz9rcQpDWS57CtERHpNehXCPcNoHGKutQY
-					// hex: 0xd43593c715fdd31c61141abd04a99fd6822c8558854ccde39a5684e7a56da27d
-					// Using the full hex key, truncating to the first 20 bytes (the first 40 hex
-					// chars)
-					H160::from_str("d43593c715fdd31c61141abd04a99fd6822c8558")
-						.expect("internal H160 is valid; qed"),
-					fp_evm::GenesisAccount {
-						balance: U256::from_str("0xffffffffffffffffffffffffffffffff")
-							.expect("internal U256 is valid; qed"),
-						code: Default::default(),
-						nonce: Default::default(),
-						storage: Default::default(),
-					},
-				);
-				map.insert(
-					// H160 address of CI test runner account
-					H160::from_str("6be02d1d3665660d22ff9624b7be0551ee1ac91b")
-						.expect("internal H160 is valid; qed"),
-					fp_evm::GenesisAccount {
-						balance: U256::from_str("0xffffffffffffffffffffffffffffffff")
-							.expect("internal U256 is valid; qed"),
-						code: Default::default(),
-						nonce: Default::default(),
-						storage: Default::default(),
-					},
-				);
-				map.insert(
-					// H160 address for benchmark usage
-					H160::from_str("1000000000000000000000000000000000000001")
-						.expect("internal H160 is valid; qed"),
-					fp_evm::GenesisAccount {
-						nonce: U256::from(1),
-						balance: U256::from(1_000_000_000_000_000_000_000_000u128),
-						storage: Default::default(),
-						code: vec![0x00],
-					},
-				);
-				map.insert(
-					// H160 address of dev account
-					// Private key :
-					// 0xb9d2ea9a615f3165812e8d44de0d24da9bbd164b65c4f0573e1ce2c8dbd9c8df
-					predefined_accounts::FAITH.into(),
-					fp_evm::GenesisAccount {
-						balance: U256::from_str("0xef000000000000000000000000000")
-							.expect("internal U256 is valid; qed"),
-						code: Default::default(),
-						nonce: Default::default(),
-						storage: Default::default(),
-					},
-				);
-				map
-			},
-			..Default::default()
+			accounts: Precompiles::used_addresses()
+				.iter()
+				.map(|&address| {
+					(
+						address,
+						GenesisAccount {
+							nonce: Default::default(),
+							balance: Default::default(),
+							storage: Default::default(),
+							code: REVERT_BYTECODE.into(),
+						},
+					)
+				})
+				.collect()..Default::default(),
 		},
 		..Default::default()
 	}
