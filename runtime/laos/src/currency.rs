d4c54560dac283e2c4bd4e580179d1a44116f32e
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2024-03-21 10:40:28 +0100

    refactor

--- runtime/laos/src/currency.rs
+++ runtime/laos/src/currency.rs
@@ -3,7 +3,7 @@ use laos_primitives::Balance;
 // Unit = the base number of indivisible units for balances
 // 18 decimals
 pub(crate) const UNIT: Balance = 1_000_000_000_000_000_000;
-pub(crate) const MILLIUNIT: Balance = UNIT / 1000;
+pub(crate) const MILLIUNIT: Balance = 1_000_000_000_000_000;
 
 // Constants in ETH terms
 const MEGAWEI: Balance = 1_000_000;
