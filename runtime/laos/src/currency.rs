28176c0124b4917b75bf879effce8965a4087878
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2024-03-21 10:28:44 +0100

    using supply factor of 100

--- runtime/laos/src/currency.rs
+++ runtime/laos/src/currency.rs
@@ -4,24 +4,20 @@ use laos_primitives::Balance;
 // 18 decimals
 pub(crate) const UNIT: Balance = 1_000_000_000_000_000_000;
 pub(crate) const MILLIUNIT: Balance = UNIT / 1000;
+pub(crate) const MICROUNIT: Balance = MILLIUNIT / 1000;
 
 // Constants in ETH terms
-pub(crate) const WEI: Balance = 1;
 pub(crate) const KILOWEI: Balance = 1_000;
+pub const MEGAWEI: Balance = 1_000_000;
 pub(crate) const GIGAWEI: Balance = 1_000_000_000;
 
-// Define storage fees as constants for clarity and reuse
 const STORAGE_ITEM_FEE: Balance = 10 * UNIT;
-const STORAGE_BYTE_FEE: Balance = 10 * MILLIUNIT;
+pub(crate) const STORAGE_BYTE_FEE: Balance = 10 * MILLIUNIT;
+pub(crate) const TRANSACTION_BYTE_FEE: Balance = 100 * GIGAWEI;
+pub(crate) const WEIGHT_FEE: Balance = 5 * MEGAWEI;
 
-/// One byte of transaction data has a fee of 1/1000 of a micro unit.
-pub(crate) const TRANSACTION_BYTE_FEE: Balance = 1 * GIGAWEI;
-/// Weight to fee conversion factor.
-pub(crate) const WEIGHT_TO_FEE: u128 = 50 * KILOWEI;
-
-/// Calculates the deposit required based on the number of items and bytes.
 pub(crate) const fn calculate_deposit(items: u32, bytes: u32) -> Balance {
-	(items as Balance) * STORAGE_ITEM_FEE + (bytes as Balance) * STORAGE_BYTE_FEE
+	items as Balance * STORAGE_ITEM_FEE + (bytes as Balance) * STORAGE_BYTE_FEE
 }
 
 #[cfg(test)]
