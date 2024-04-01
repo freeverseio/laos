0644401591ae2eb6e219e84bc1603d46f8830eed
Christian Cuaresma <christian@freeverse.io>
2024-04-01 16:29:18 +0200

    add license to source files

--- runtime/laos/src/configs/cumulus_parachain_system.rs
+++ runtime/laos/src/configs/cumulus_parachain_system.rs
@@ -1,4 +1,21 @@
+// Copyright 2023-2024 Freverse.io
+// This file is part of LAOS.
+
+// LAOS is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// LAOS is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with LAOS.  If not, see <http://www.gnu.org/licenses/>.
+
 use crate::{Block, DmpQueue, InherentDataExt, ParachainInfo, Runtime, RuntimeEvent, XcmpQueue};
+
 use frame_support::{parameter_types, weights::Weight};
 
 use laos_primitives::MAXIMUM_BLOCK_WEIGHT;
