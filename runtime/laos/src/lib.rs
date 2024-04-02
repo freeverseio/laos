9f0a51a821ad903ad6b97275e7219128bc98e078
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2024-04-02 09:55:33 +0200

    fix compilation

--- runtime/laos/src/lib.rs
+++ runtime/laos/src/lib.rs
@@ -21,7 +21,7 @@ use frame_support::{
 };
 use frame_system::EnsureRoot;
 pub use laos_primitives::{
-	AccountId, AuraId, Balance, BlockNumber, Hash, Header, Index, Nonce, Signature,
+	AccountId, AuraId, Balance, BlockNumber, Hash, Header, Nonce, Signature,
 };
 pub use pallet_evm_evolution_collection_factory::REVERT_BYTECODE;
 pub use pallet_parachain_staking::{InflationInfo, Range};
