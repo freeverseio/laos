e5fcb2be3503a5dcf325dcd87edaf0937bf1f0d1
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2024-04-02 15:08:54 +0200

    xcm_router

---
+++ runtime/laos/src/types/xcm_router.rs
@@ -0,0 +1,13 @@
+/// The means for routing XCM messages which are not for local execution into the right message
+/// queues.
+#[cfg(not(test))]
+pub type XcmRouter = staging_xcm_builder::WithUniqueTopic<(
+	// Two routers - use UMP to communicate with the relay chain:
+	cumulus_primitives_utility::ParentAsUmp<crate::ParachainSystem, (), ()>,
+	// ..and XCMP to communicate with the sibling chains.
+	crate::XcmpQueue,
+)>;
+
+/// Use different router in `xcm-simulator` tests.
+#[cfg(test)]
+pub type XcmRouter = crate::tests::ParachainXcmRouter<ParachainInfo>;
