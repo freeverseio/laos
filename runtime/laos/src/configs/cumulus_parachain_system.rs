616681883f9c6d5739ad71ad14e82d31d6cb0513
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2024-04-03 09:44:16 +0200

    refactoring (#529)

--- runtime/laos/src/configs/cumulus_parachain_system.rs
+++ runtime/laos/src/configs/cumulus_parachain_system.rs
@@ -1,26 +1,23 @@
-use crate::{
-	Block, DmpQueue, InherentDataExt, RelayNumberStrictlyIncreases, Runtime, RuntimeEvent,
-	XcmpQueue,
-};
+use crate::{Block, DmpQueue, InherentDataExt, ParachainInfo, Runtime, RuntimeEvent, XcmpQueue};
 use frame_support::{parameter_types, weights::Weight};
 
 use laos_primitives::MAXIMUM_BLOCK_WEIGHT;
 
 parameter_types! {
-	pub const ReservedXcmpWeight: Weight = MAXIMUM_BLOCK_WEIGHT.saturating_div(4); // TODO
-	pub const ReservedDmpWeight: Weight = MAXIMUM_BLOCK_WEIGHT.saturating_div(4); // TODO
+	pub const ReservedXcmpWeight: Weight = MAXIMUM_BLOCK_WEIGHT.saturating_div(4);
+	pub const ReservedDmpWeight: Weight = MAXIMUM_BLOCK_WEIGHT.saturating_div(4);
 }
 
 impl cumulus_pallet_parachain_system::Config for Runtime {
 	type RuntimeEvent = RuntimeEvent;
 	type OnSystemEvent = ();
-	type SelfParaId = parachain_info::Pallet<Runtime>;
+	type SelfParaId = ParachainInfo;
 	type OutboundXcmpMessageSource = XcmpQueue;
 	type DmpMessageHandler = DmpQueue;
 	type ReservedDmpWeight = ReservedDmpWeight;
 	type XcmpMessageHandler = XcmpQueue;
 	type ReservedXcmpWeight = ReservedXcmpWeight;
-	type CheckAssociatedRelayNumber = RelayNumberStrictlyIncreases;
+	type CheckAssociatedRelayNumber = cumulus_pallet_parachain_system::RelayNumberStrictlyIncreases;
 }
 
 struct CheckInherents;
