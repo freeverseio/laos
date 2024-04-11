ee468265c6b7018d2968762ff4b3715be4b4b6e6
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2024-04-11 17:55:12 +0200

    Add Support for Klaos-Nova Chain Specification (#560)
    * from genesis klaos nova
    
    * set nova
    
    * nova specs raw
    
    * add klaos-nova

--- node/src/command.rs
+++ node/src/command.rs
@@ -55,6 +55,9 @@ fn load_spec(id: &str) -> std::result::Result<Box<dyn ChainSpec>, String> {
 		"klaos" => Box::new(chain_spec::klaos::ChainSpec::from_json_bytes(
 			&include_bytes!("../../specs/klaos.raw.json")[..],
 		)?),
+		"klaos-nova" => Box::new(chain_spec::klaos::ChainSpec::from_json_bytes(
+			&include_bytes!("../../specs/klaos-nova.raw.json")[..],
+		)?),
 		"giedi" => Box::new(chain_spec::klaos::ChainSpec::from_json_bytes(
 			&include_bytes!("../../specs/giedi.raw.json")[..],
 		)?),
