e5dbbc629b1b4df08922b3d3037760de56c70649
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2024-05-31 09:43:05 +0200

    commented out not used benchmarks

--- runtime/laos/src/benchmarks.rs
+++ runtime/laos/src/benchmarks.rs
@@ -15,19 +15,19 @@
 // along with LAOS.  If not, see <http://www.gnu.org/licenses/>.
 
 frame_benchmarking::define_benchmarks!(
-	[frame_system, SystemBench::<Runtime>]
+	// [frame_system, SystemBench::<Runtime>]
 	[pallet_timestamp, Timestamp]
 	[pallet_sudo, Sudo]
 	[pallet_utility, Utility]
 	[pallet_multisig, Multisig]
 	[pallet_proxy, Proxy]
-	[pallet_balances, Balances]
+	// [pallet_balances, Balances]
 	[pallet_vesting, Vesting]
 	[pallet_session, SessionBench::<Runtime>] // TODO check why SessionBench::<Runtime>
 	[pallet_parachain_staking, ParachainStaking]
 	[cumulus_pallet_xcmp_queue, XcmpQueue]
 	[pallet_evm, EVM]
-	[pallet_laos_evolution, LaosEvolution]
-	[pallet_asset_metadata_extender, AssetMetadataExtender]
+	// [pallet_laos_evolution, LaosEvolution]
+	// [pallet_asset_metadata_extender, AssetMetadataExtender]
 	// TODO pallet_xcm?
 );
