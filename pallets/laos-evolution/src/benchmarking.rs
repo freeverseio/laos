7ee96f210dd1cd916a0a163507b6ddc93b9aef9f
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2024-06-20 11:29:58 +0200

    new mock

--- pallets/laos-evolution/src/benchmarking.rs
+++ pallets/laos-evolution/src/benchmarking.rs
@@ -27,8 +27,92 @@ use crate::Pallet as LaosEvolution;
 use fp_evm::Transfer;
 use frame_benchmarking::v2::*;
 use sp_std::{vec, vec::Vec};
+use precompile_utils::prelude::{ Address };
+use pallet_evm::{Context, Log, ExitError, ExitReason, PrecompileHandle};
+use sp_core::{ H256 , H160};
 
-struct MockPrecompileHandle;
+pub struct MockHandle {
+	pub input: Vec<u8>,
+	pub gas_limit: Option<u64>,
+	pub context: Context,
+	pub is_static: bool,
+	pub gas_used: u64,
+	pub logs: Vec<Log>,
+	pub code_address: H160,
+}
+
+impl MockHandle {
+	pub fn new(input: Vec<u8>, gas_limit: Option<u64>, context: Context) -> Self {
+		Self {
+			input,
+			gas_limit,
+			context,
+			is_static: false,
+			gas_used: 0,
+			logs: vec![],
+			code_address: H160::zero(),
+		}
+	}
+}
+
+impl PrecompileHandle for MockHandle {
+	/// Perform subcall in provided context.
+	/// Precompile specifies in which context the subcall is executed.
+	fn call(
+		&mut self,
+		_: H160,
+		_: Option<Transfer>,
+		_: Vec<u8>,
+		_: Option<u64>,
+		_: bool,
+		_: &Context,
+	) -> (ExitReason, Vec<u8>) {
+		unimplemented!()
+	}
+
+	fn record_cost(&mut self, cost: u64) -> Result<(), ExitError> {
+		self.gas_used += cost;
+		Ok(())
+	}
+
+	fn record_external_cost(&mut self, _: Option<u64>, _: Option<u64>) -> Result<(), ExitError> {
+		Ok(())
+	}
+
+	fn refund_external_cost(&mut self, _: Option<u64>, _: Option<u64>) {}
+
+	fn log(&mut self, address: H160, topics: Vec<H256>, data: Vec<u8>) -> Result<(), ExitError> {
+		let log = Log { address, topics, data };
+		self.logs.push(log);
+		Ok(())
+	}
+
+	fn remaining_gas(&self) -> u64 {
+		unimplemented!()
+	}
+
+	fn code_address(&self) -> H160 {
+		self.code_address
+	}
+
+	fn input(&self) -> &[u8] {
+		&self.input
+	}
+
+	fn context(&self) -> &Context {
+		&self.context
+	}
+
+	fn is_static(&self) -> bool {
+		self.is_static
+	}
+
+	fn gas_limit(&self) -> Option<u64> {
+		self.gas_limit
+	}
+}
+
+struct MockPrecompileHandle{}
 impl pallet_evm::PrecompileHandle for MockPrecompileHandle {
 	fn call(
 		&mut self,
@@ -102,7 +186,7 @@ mod benchmarks {
 	// 	{
 	// 		let _ = EvolutionCollectionFactoryPrecompile::<T>::create_collection(
 	// 			&mut handle,
-	// 			H160::zero(),
+	// 			Address::from(H160::zero()),
 	// 		);
 	// 	}
 	// }
@@ -112,7 +196,7 @@ mod benchmarks {
 		let caller: T::AccountId = whitelisted_caller();
 		let owner = caller.clone();
 		let collection_id = LaosEvolution::<T>::create_collection(owner).unwrap();
-		let mut handle = MockPrecompileHandle;
+		let mut handle = MockPrecompileHandle{};
 
 		#[block]
 		{
