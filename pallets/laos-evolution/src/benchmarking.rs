32624b674c5098ab1467ee7c6bd8e8ec42ead8a1
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2024-06-20 11:30:27 +0200

    new mock

--- pallets/laos-evolution/src/benchmarking.rs
+++ pallets/laos-evolution/src/benchmarking.rs
@@ -178,18 +178,18 @@ impl pallet_evm::PrecompileHandle for MockPrecompileHandle {
 mod benchmarks {
 	use super::*;
 
-	// #[benchmark]
-	// fn precompile_create_collection() {
-	// 	let mut handle = MockPrecompileHandle;
+	#[benchmark]
+	fn precompile_create_collection() {
+		let mut handle = MockHandle::new(vec![], None, Context::default());
 
-	// 	#[block]
-	// 	{
-	// 		let _ = EvolutionCollectionFactoryPrecompile::<T>::create_collection(
-	// 			&mut handle,
-	// 			Address::from(H160::zero()),
-	// 		);
-	// 	}
-	// }
+		#[block]
+		{
+			let _ = EvolutionCollectionFactoryPrecompile::<T>::create_collection(
+				&mut handle,
+				Address::from(H160::zero()),
+			);
+		}
+	}
 
 	#[benchmark]
 	fn precompile_owner() {
