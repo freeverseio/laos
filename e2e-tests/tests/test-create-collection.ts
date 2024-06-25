3f4504c699edb65bf935aae04a479994ec4d1887
magecnion <magecnion@gmail.com>
2024-06-25 18:11:34 +0200

    WIP add owner estimateGast

--- e2e-tests/tests/test-create-collection.ts
+++ e2e-tests/tests/test-create-collection.ts
@@ -33,53 +33,60 @@ describeWithExistingNode("Frontier RPC (Create Collection)", (context) => {
 		this.timeout(70000);
 
 		const collectionContract = await createCollection(context);
+		console.log("collectionContract", collectionContract);
 		testCollectionContract = collectionContract;
 
-		const owner = await collectionContract.methods.owner().call();
-		expect(owner).to.be.eq(GENESIS_ACCOUNT);
-	});
-
-	step("when collection is created event is emitted", async function () {
-		this.timeout(70000);
-
-		const result = await contract.methods.createCollection(GENESIS_ACCOUNT).send({
+		const estimateGas = await collectionContract.methods.owner().estimatedGas({
 			from: GENESIS_ACCOUNT,
 			gas: GAS_LIMIT,
 			gasPrice: GAS_PRICE,
 		});
-		expect(result.status).to.be.eq(true);
-
-		expect(Object.keys(result.events).length).to.be.eq(1);
-		expect(context.web3.utils.isAddress(result.events.NewCollection.returnValues._collectionAddress)).to.be.eq(
-			true
-		);
-		testCollectionAddress = result.events.NewCollection.returnValues._collectionAddress;
-		expect(result.events.NewCollection.returnValues._owner).to.be.eq(GENESIS_ACCOUNT);
-
-		// event topics
-		expect(result.events.NewCollection.raw.topics.length).to.be.eq(2);
-		expect(result.events.NewCollection.raw.topics[0]).to.be.eq(SELECTOR_LOG_NEW_COLLECTION);
-		expect(result.events.NewCollection.raw.topics[1]).to.be.eq(
-			context.web3.utils.padLeft(GENESIS_ACCOUNT.toLowerCase(), 64)
-		);
-
-		// event data
-		expect(result.events.NewCollection.raw.data.toLowerCase()).to.be.eq(
-			context.web3.utils.padLeft(result.events.NewCollection.returnValues._collectionAddress, 64).toLowerCase()
-		);
+		console.log("estimateGas", estimateGas);
+		expect(estimateGas).to.be.eq(226);
+		const owner = await collectionContract.methods.owner().call();
+		console.log("owner", owner);
+		expect(owner).to.be.eq(GENESIS_ACCOUNT);
 	});
 
-	step("when collection is created, bytecode is inserted in the storage", async function () {
-		expect(await context.web3.eth.getCode(testCollectionContract.options.address)).to.be.eq(REVERT_BYTECODE);
-		expect(await context.web3.eth.getCode(testCollectionAddress)).to.be.eq(REVERT_BYTECODE);
+	// step("when collection is created event is emitted", async function () {
+	// 	this.timeout(70000);
 
-		// non-contract address doesn't have any code
-		expect(await context.web3.eth.getCode(GENESIS_ACCOUNT)).to.be.eq("0x");
-	});
+	// 	const result = await contract.methods.createCollection(GENESIS_ACCOUNT).send({
+	// 		from: GENESIS_ACCOUNT,
+	// 		gas: GAS_LIMIT,
+	// 		gasPrice: GAS_PRICE,
+	// 	});
+	// 	expect(result.status).to.be.eq(true);
+
+	// 	expect(Object.keys(result.events).length).to.be.eq(1);
+	// 	expect(context.web3.utils.isAddress(result.events.NewCollection.returnValues._collectionAddress)).to.be.eq(
+	// 		true
+	// 	);
+	// 	testCollectionAddress = result.events.NewCollection.returnValues._collectionAddress;
+	// 	expect(result.events.NewCollection.returnValues._owner).to.be.eq(GENESIS_ACCOUNT);
+
+	// 	// event topics
+	// 	expect(result.events.NewCollection.raw.topics.length).to.be.eq(2);
+	// 	expect(result.events.NewCollection.raw.topics[0]).to.be.eq(SELECTOR_LOG_NEW_COLLECTION);
+	// 	expect(result.events.NewCollection.raw.topics[1]).to.be.eq(
+	// 		context.web3.utils.padLeft(GENESIS_ACCOUNT.toLowerCase(), 64)
+	// 	);
+
+	// 	// event data
+	// 	expect(result.events.NewCollection.raw.data.toLowerCase()).to.be.eq(
+	// 		context.web3.utils.padLeft(result.events.NewCollection.returnValues._collectionAddress, 64).toLowerCase()
+	// 	);
+	// });
+
+	// step("when collection is created, bytecode is inserted in the storage", async function () {
+	// 	expect(await context.web3.eth.getCode(testCollectionContract.options.address)).to.be.eq(REVERT_BYTECODE);
+	// 	expect(await context.web3.eth.getCode(testCollectionAddress)).to.be.eq(REVERT_BYTECODE);
+
+	// 	// non-contract address doesn't have any code
+	// 	expect(await context.web3.eth.getCode(GENESIS_ACCOUNT)).to.be.eq("0x");
+	// });
 
 	step("createCollection can call estimateGas", async function () {
-		this.timeout(70000);
-
 		const contract = new context.web3.eth.Contract(EVOLUTION_COLLETION_FACTORY_ABI, CONTRACT_ADDRESS, {
 			from: GENESIS_ACCOUNT,
 			gasPrice: GAS_PRICE,
@@ -94,9 +101,6 @@ describeWithExistingNode("Frontier RPC (Create Collection)", (context) => {
 			gasPrice: GAS_PRICE,
 			nonce: nonce++,
 		});
-		
-		expect(estimatedGas).to.be.a('number');
-		
-		
+		expect(estimatedGas).to.be.eq(43228);
 	});
 });
