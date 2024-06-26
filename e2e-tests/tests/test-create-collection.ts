5d71b02a841f21a033bb39f1a52e3261d14a17a5
magecnion <magecnion@gmail.com>
2024-06-26 09:09:41 +0200

    fix test

--- e2e-tests/tests/test-create-collection.ts
+++ e2e-tests/tests/test-create-collection.ts
@@ -79,7 +79,7 @@ describeWithExistingNode("Frontier RPC (Create Collection)", (context) => {
 
 	step("owner call can estimate gas", async function () {
 		const estimateGas = await testCollectionContract.methods.owner().estimateGas();
-		expect(estimateGas).to.be.eq(22433);
+		expect(estimateGas).to.be.eq(22428);
 	});
 
 	step("create collection call can estimate gas", async function () {
