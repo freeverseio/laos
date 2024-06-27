4a9a103078bcaa7a8041eed844f9134aaed27f9c
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2024-06-27 15:53:24 +0200

    fix tests

--- runtime/laos/src/configs/evm.rs
+++ runtime/laos/src/configs/evm.rs
@@ -180,7 +180,7 @@ mod tests {
 				);
 				assert_eq!(
 					call_result.actual_weight.unwrap(),
-					Weight::from_parts(1039711000, 11641)
+					Weight::from_parts(1037811000, 11622)
 				);
 
 				// check gas
@@ -194,18 +194,18 @@ mod tests {
 					<Runtime as pallet_evm::Config>::GasWeightMapping::weight_to_gas(
 						call_result.actual_weight.unwrap()
 					),
-					41588
+					41512
 				);
 
 				// check weights from benchmarking
 				let weights_from_benchmarking =
 					weights::pallet_laos_evolution::WeightInfo::<Runtime>::precompile_create_collection();
-				assert_eq!(weights_from_benchmarking, Weight::from_parts(628336000, 3_840));
+				assert_eq!(weights_from_benchmarking, Weight::from_parts(626439000, 3_840));
 				assert_eq!(
 					<Runtime as pallet_evm::Config>::GasWeightMapping::weight_to_gas(
 						weights_from_benchmarking
 					),
-					25133
+					25057
 				);
 			});
 	}
@@ -256,10 +256,7 @@ mod tests {
 					wrong_address_call_result.actual_weight.unwrap(),
 					Weight::from_parts(402_186_000, 5_266)
 				);
-				assert_eq!(
-					call_result.actual_weight.unwrap(),
-					Weight::from_parts(434_111_000, 5_585)
-				);
+				assert_eq!(call_result.actual_weight.unwrap(), Weight::from_parts(435711000, 5601));
 
 				// check gas
 				assert_eq!(
@@ -272,18 +269,18 @@ mod tests {
 					<Runtime as pallet_evm::Config>::GasWeightMapping::weight_to_gas(
 						call_result.actual_weight.unwrap()
 					),
-					17364
+					17428
 				);
 
 				// check weights from benchmarking
 				let weights_from_benchmarking =
 					weights::pallet_laos_evolution::WeightInfo::<Runtime>::precompile_owner();
-				assert_eq!(weights_from_benchmarking, Weight::from_parts(31653000, 3_509));
+				assert_eq!(weights_from_benchmarking, Weight::from_parts(31714000, 3_509));
 				assert_eq!(
 					<Runtime as pallet_evm::Config>::GasWeightMapping::weight_to_gas(
 						weights_from_benchmarking
 					),
-					1266
+					1268
 				);
 			});
 	}
