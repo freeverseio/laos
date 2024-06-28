b2101127d7d2ce68c3b581fc085adceaac540741
magecnion <magecnion@gmail.com>
2024-06-28 10:24:11 +0200

    fix gas tests

--- precompile/asset-metadata-extender/src/tests.rs
+++ precompile/asset-metadata-extender/src/tests.rs
@@ -217,7 +217,7 @@ fn create_token_uri_extension_records_cost() {
 		// `create_token_uri_extension` weight + log cost
 		precompiles()
 			.prepare_test(H160::from_str(TEST_CLAIMER).unwrap(), H160(PRECOMPILE_ADDRESS), input)
-			.expect_cost(362819040) // [`WeightToGas`] set to 1:1 in mock
+			.expect_cost(362211111) // [`WeightToGas`] set to 1:1 in mock
 			.execute_some();
 	})
 }
@@ -251,7 +251,7 @@ fn update_token_uri_extension_records_cost() {
 		// `create_token_uri_extension` weight + log cost
 		precompiles()
 			.prepare_test(H160::from_str(TEST_CLAIMER).unwrap(), H160(PRECOMPILE_ADDRESS), input)
-			.expect_cost(135678398) // [`WeightToGas`] set to 1:1 in mock
+			.expect_cost(135266175) // [`WeightToGas`] set to 1:1 in mock
 			.execute_some();
 	})
 }
