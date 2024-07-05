f4fe54188b006e49f8a60047f707beb2a19a9ea7
magecnion <magecnion@gmail.com>
2024-07-05 10:24:37 +0200

    remove ununsed var

--- e2e-tests/tests/config.ts
+++ e2e-tests/tests/config.ts
@@ -9,7 +9,6 @@ export const RUNTIME_SPEC_NAME = "laos";
 export const RUNTIME_SPEC_VERSION = 1701;
 export const RUNTIME_IMPL_VERSION = 0;
 export const LOCAL_NODE_URL = "http://127.0.0.1:9999";
-export const NODE_URL = "wss://rpc.laossun.gorengine.com";
 
 // Chain config
 export const CHAIN_ID = 667;
