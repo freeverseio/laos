fb0366d314a4b05406e3d923e4f84947b4322e0e
luispdm <17044119+luispdm@users.noreply.github.com>
2024-07-09 16:28:12 +0200

    vesting precompile mock and start tests

--- precompiles/vesting/src/lib.rs
+++ precompiles/vesting/src/lib.rs
@@ -18,13 +18,14 @@
 
 #![cfg_attr(not(feature = "std"), no_std)]
 use fp_evm::ExitError;
-use frame_support::{pallet_prelude::Weight, DefaultNoBound, traits::tokens::currency::Currency};
+use frame_support::{pallet_prelude::Weight, traits::tokens::currency::Currency, DefaultNoBound};
 use frame_system::{pallet_prelude::BlockNumberFor, RawOrigin};
 use pallet_evm::GasWeightMapping;
 use pallet_vesting::{Config, Pallet};
-use precompile_utils::{precompile, prelude::{
-	revert, solidity, Address, EvmResult, PrecompileHandle,
-}};
+use precompile_utils::{
+	precompile,
+	prelude::{revert, solidity, Address, EvmResult, PrecompileHandle},
+};
 use scale_info::prelude::{format, string::String};
 use sp_core::{H160, U256};
 use sp_runtime::{
@@ -33,7 +34,9 @@ use sp_runtime::{
 };
 use sp_std::vec::Vec;
 
-type BalanceOf<Runtime> = <<Runtime as Config>::Currency as Currency<<Runtime as frame_system::Config>::AccountId>>::Balance;
+type BalanceOf<Runtime> = <<Runtime as Config>::Currency as Currency<
+	<Runtime as frame_system::Config>::AccountId,
+>>::Balance;
 
 #[derive(Default, solidity::Codec)]
 pub struct VestingInfo {
@@ -60,7 +63,10 @@ where
 {
 	#[precompile::public("vesting(address)")]
 	#[precompile::view]
-	pub fn vesting(handle: &mut impl PrecompileHandle, account: Address) -> EvmResult<Vec<VestingInfo>> {
+	pub fn vesting(
+		handle: &mut impl PrecompileHandle,
+		account: Address,
+	) -> EvmResult<Vec<VestingInfo>> {
 		// TODO super::register_cost::<Runtime>(handle, Runtime::WeightInfo::precompile_vesting())?;
 
 		match Pallet::<Runtime>::vesting(Runtime::convert_back(account.into())) {
@@ -68,7 +74,7 @@ where
 				let mut output: Vec<VestingInfo> = Vec::with_capacity(v.len());
 
 				for i in v {
-					output.push(VestingInfo{
+					output.push(VestingInfo {
 						locked: i.locked().into(),
 						per_block: i.per_block().into(),
 						starting_block: i.starting_block().into(),
@@ -85,9 +91,11 @@ where
 	pub fn vest(handle: &mut impl PrecompileHandle) -> EvmResult<()> {
 		// TODO super::register_cost::<Runtime>(handle, Runtime::WeightInfo::precompile_vest())?;
 
-		match Pallet::<Runtime>::vest(<Runtime as frame_system::Config>::RuntimeOrigin::from(RawOrigin::from(Some(Runtime::convert_back(handle.context().caller))))) {
+		match Pallet::<Runtime>::vest(<Runtime as frame_system::Config>::RuntimeOrigin::from(
+			RawOrigin::from(Some(Runtime::convert_back(handle.context().caller))),
+		)) {
 			Ok(_) => Ok(()),
-			Err(err) => Err(revert(convert_dispatch_error_to_string(err)))
+			Err(err) => Err(revert(convert_dispatch_error_to_string(err))),
 		}
 	}
 
@@ -95,12 +103,15 @@ where
 	pub fn vest_other(handle: &mut impl PrecompileHandle, account: Address) -> EvmResult<()> {
 		// TODO super::register_cost::<Runtime>(handle, Runtime::WeightInfo::precompile_vest_other())?;
 
-		let origin = <Runtime as frame_system::Config>::RuntimeOrigin::from(RawOrigin::from(Some(Runtime::convert_back(handle.context().caller))));
+		let origin = <Runtime as frame_system::Config>::RuntimeOrigin::from(RawOrigin::from(Some(
+			Runtime::convert_back(handle.context().caller),
+		)));
 		let account_id = Runtime::convert_back(account.into());
-		let target = <<Runtime as frame_system::Config>::Lookup as StaticLookup>::unlookup(account_id);
+		let target =
+			<<Runtime as frame_system::Config>::Lookup as StaticLookup>::unlookup(account_id);
 		match Pallet::<Runtime>::vest_other(origin, target) {
 			Ok(_) => Ok(()),
-			Err(err) => Err(revert(convert_dispatch_error_to_string(err)))
+			Err(err) => Err(revert(convert_dispatch_error_to_string(err))),
 		}
 	}
 }
@@ -116,7 +127,7 @@ fn convert_dispatch_error_to_string(err: DispatchError) -> String {
 // 	handle: &mut impl PrecompileHandle,
 // 	weight: Weight,
 // ) -> Result<(), ExitError> {
-// 	let required_gas = <pallet_evm::FixedGasWeightMapping<Runtime> as GasWeightMapping>::weight_to_gas(weight);
+// 	let required_gas = Runtime::GasWeightMapping::weight_to_gas(weight);
 // 	let remaining_gas = handle.remaining_gas();
 // 	if required_gas > remaining_gas {
 // 		return Err(ExitError::OutOfGas);
@@ -126,7 +137,7 @@ fn convert_dispatch_error_to_string(err: DispatchError) -> String {
 // 	Ok(())
 // }
 
-// #[cfg(test)]
-// mod mock;
-// #[cfg(test)]
-// mod tests;
+#[cfg(test)]
+mod mock;
+#[cfg(test)]
+mod tests;
---
+++ precompiles/vesting/src/tests.rs
@@ -0,0 +1,35 @@
+// Copyright 2023-2024 Freeverse.io
+// This file is part of LAOS.
+
+// LAOS is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// LAOS is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with LAOS.  If not, see <http://www.gnu.org/licenses/>.
+
+//! Living assets precompile tests.
+
+use super::*;
+use mock::*;
+use precompile_utils::testing::{Alice, Precompile1, PrecompileTesterExt};
+
+/// Get precompiles from the mock.
+fn precompiles() -> LaosPrecompiles<Test> {
+	PrecompilesInstance::get()
+}
+
+#[test]
+fn vest_reverts_no_vested_funds() {
+	new_test_ext().execute_with(|| {
+		precompiles()
+			.prepare_test(Alice, Precompile1, PrecompileCall::vest {})
+			.execute_reverts(|r| r == b"NotVesting");
+	});
+}
