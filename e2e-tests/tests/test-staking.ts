bbb64467c73ee245325e5075a19ff49fc8ae6bea
magecnion <magecnion@gmail.com>
2024-07-23 15:55:35 +0200

    alith can join as candidate

--- e2e-tests/tests/test-staking.ts
+++ e2e-tests/tests/test-staking.ts
@@ -1,18 +1,8 @@
 import {
-    addressToCollectionId,
-    createCollection,
     describeWithExistingNode,
-    extractRevertReason,
-    slotAndOwnerToTokenId,
 } from "./util";
 import {
     GAS_LIMIT,
-    FAITH,
-    SELECTOR_LOG_EVOLVED_WITH_EXTERNAL_TOKEN_URI,
-    SELECTOR_LOG_MINTED_WITH_EXTERNAL_TOKEN_URI,
-    SELECTOR_LOG_OWNERSHIP_TRANSFERRED,
-    SELECTOR_LOG_PUBLIC_MINTING_ENABLED,
-    SELECTOR_LOG_PUBLIC_MINTING_DISABLED,
     ALITH,
     ALITH_PRIVATE_KEY,
     STAKING_ABI,
@@ -22,7 +12,6 @@ import {
 } from "./config";
 import { expect } from "chai";
 import Contract from "web3-eth-contract";
-import BN from "bn.js";
 import { step } from "mocha-steps";
 import { Keyring } from "@polkadot/api";
 
@@ -48,8 +37,7 @@ describeWithExistingNode("Frontier RPC (Staking)", (context) => {
 
         let nonce = await context.web3.eth.getTransactionCount(ALITH);
         const result = await contract.methods.joinCandidates(BigInt(20000) * UNIT, 0).send({ from: ALITH, gas: GAS_LIMIT, nonce: nonce++ });
-        console.log(`result: ${result}`);
+        expect(result.status).to.be.eq(true);
     });
-
 });
 
