d9b31df2651e98adbf4306d4c170c0770ef75677
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2024-08-08 10:40:08 +0200

    all settingS

--- runtime/laos/src/configs/collective_council.rs
+++ runtime/laos/src/configs/collective_council.rs
@@ -1,14 +1,18 @@
-use crate::{ AccountId, BlockNumber, Runtime, EnsureRoot, RuntimeCall, RuntimeOrigin, RuntimeEvent };
-use frame_support::parameter_types;
+use crate::{
+	AccountId, BlockNumber, EnsureRoot, Runtime, RuntimeCall, RuntimeEvent, RuntimeOrigin,
+};
+use frame_support::{pallet_prelule::Weight, parameter_types};
 use parachains_common::DAYS;
+use sp_runtime::Perbill;
 
 const COUNCIL_MOTION_DURATION: BlockNumber = 7 * DAYS;
 
 parameter_types! {
-    pub const CouncilMotionDuration: BlockNumber = COUNCIL_MOTION_DURATION;
+	pub const CouncilMotionDuration: BlockNumber = COUNCIL_MOTION_DURATION;
 	pub const CouncilMaxProposals: u32 = 7;
 	pub const CouncilMaxMembers: u32 = 20;
-    pub const MaxProposalWeight: Weight = Perbill::from_percent(50) * RuntimeBlockWeights::get().max_block;
+	pub const MaxProposalWeight: Weight = Perbill::from_percent(50) * RuntimeBlockWeights::get().max_block;
+	pub const MaxCollectivesProposalWeight: Weight = Perbill::from_percent(50) * RuntimeBlockWeights::get().max_block;
 }
 
 impl pallet_collective::Config for Runtime {
