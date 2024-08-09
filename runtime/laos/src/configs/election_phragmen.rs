64bd6709fc29c24d70ec573bd3e1a03a7d475195
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2024-08-09 15:15:05 +0200

    some deps

--- runtime/laos/src/configs/election_phragmen.rs
+++ runtime/laos/src/configs/election_phragmen.rs
@@ -1,7 +1,29 @@
-use crate::{weights, Balances, Council, CurrencyToVote, ToTreasury};
+use crate::{Balances, Council, Runtime, RuntimeEvent, BlockNumber, currency::UNIT, Balance};
+use frame_support::parameter_types;
+use parachains_common::DAYS;
+
+#[cfg(feature = "fast-mode")]
+pub const TERM_DURATION: BlockNumber = 4 * HOURS;
+#[cfg(not(feature = "fast-mode"))]
+pub const TERM_DURATION: BlockNumber = 28 * DAYS;
+
+#[cfg(feature = "fast-mode")]
+pub const ELECTION_VOTING_LOCK_DURATION: BlockNumber = 30 * MINUTES;
+#[cfg(not(feature = "fast-mode"))]
+pub const ELECTION_VOTING_LOCK_DURATION: BlockNumber = 28 * DAYS;
+
+parameter_types! {
+    pub const CandidacyBond: Balance = 1000 * UNIT;
+	pub TermDuration: BlockNumber = TERM_DURATION;
+	pub VotingLockPeriod: BlockNumber = ELECTION_VOTING_LOCK_DURATION;
+	pub const DesiredMembers: u32 = 9;
+	pub const DesiredRunnersUp: u32 = 20;
+	pub const MaxCandidates: u32 = 30;
+	pub const MaxVoters: u32 = 200;
+	pub const MaxVotesPerVoter: u32 = 8;
+}
 
 impl pallet_elections_phragmen::Config for Runtime {
-	type Balance = Balance;
 	/// How much should be locked up in order to submit one's candidacy.
 	type CandidacyBond = CandidacyBond;
 	type ChangeMembers = Council;
@@ -16,13 +38,14 @@ impl pallet_elections_phragmen::Config for Runtime {
 	type MaxCandidates = MaxCandidates;
 	type MaxVoters = MaxVoters;
 	type MaxVotesPerVoter = MaxVotesPerVoter;
+	type PalletId = (); // TODO
 	type RuntimeEvent = RuntimeEvent;
-	type RuntimeFreezeReason = RuntimeFreezeReason;
-	type RuntimeHoldReason = RuntimeHoldReason;
 	/// How long each seat is kept. This defines the next block number at which
 	/// an election round will happen. If set to zero, no elections are ever
 	/// triggered and the module will be in passive mode.
 	type TermDuration = TermDuration;
-	type VotingLockPeriod = VotingLockPeriod;
-	type WeightInfo = weights::pallet_elections_phragmen::WeightInfo<Runtime>;
+	type VotingBondBase = (); // TODO
+	type VotingBondFactor = (); // TODO
+	type KickedMember = (); // TODO
+	type WeightInfo = (); // weights::pallet_elections_phragmen::WeightInfo<Runtime>;
 }
