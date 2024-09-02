61e16b044a0d4f3f65ac79b36dfa4207676ca60f
magecnion <magecnion@gmail.com>
2024-09-02 17:29:22 +0100

    Review #750 (#755)
    * add treasury module to identity configuration; use default idendity weights; use parameter_types macro instead of Const
    
    * improve comments
    
    * fmt
    
    * increase e2e timeout

--- runtime/laos/src/configs/identity.rs
+++ runtime/laos/src/configs/identity.rs
@@ -1,8 +1,10 @@
-use crate::{currency, AccountId, Balances, Runtime, RuntimeEvent, Signature};
+use crate::{
+	currency::calculate_deposit, AccountId, Balance, Balances, Runtime, RuntimeEvent, Signature,
+	Treasury,
+};
 use frame_support::parameter_types;
 use frame_system::EnsureRoot;
 use parachains_common::DAYS;
-use sp_core::ConstU128;
 
 parameter_types! {
 	pub const MaxSubAccounts: u32 = 100;
@@ -11,6 +13,9 @@ parameter_types! {
 	pub const PendingUsernameExpiration: u32 = 7 * DAYS;
 	pub const MaxSuffixLength: u32 = 7;
 	pub const MaxUsernameLength: u32 = 32;
+	pub const BasicDeposit: Balance = calculate_deposit(1, 258);
+	pub const ByteDeposit: Balance = calculate_deposit(0, 1);
+	pub const SubAccountDeposit: Balance = calculate_deposit(0, 53);
 }
 
 type IdentityForceOrigin = EnsureRoot<AccountId>;
@@ -20,15 +25,15 @@ impl pallet_identity::Config for Runtime {
 	type RuntimeEvent = RuntimeEvent;
 	type Currency = Balances;
 	// Add one item in storage and take 258 bytes
-	type BasicDeposit = ConstU128<{ currency::calculate_deposit(1, 258) }>;
+	type BasicDeposit = BasicDeposit;
 	// Does not add any item to the storage but takes 1 bytes
-	type ByteDeposit = ConstU128<{ currency::calculate_deposit(0, 1) }>;
+	type ByteDeposit = ByteDeposit;
 	// Add one item in storage and take 53 bytes
-	type SubAccountDeposit = ConstU128<{ currency::calculate_deposit(1, 53) }>;
+	type SubAccountDeposit = SubAccountDeposit;
 	type MaxSubAccounts = MaxSubAccounts;
 	type IdentityInformation = pallet_identity::legacy::IdentityInfo<MaxAdditionalFields>;
 	type MaxRegistrars = MaxRegistrars;
-	type Slashed = (); // TODO: Treasury;
+	type Slashed = Treasury;
 	type ForceOrigin = IdentityForceOrigin;
 	type RegistrarOrigin = IdentityRegistrarOrigin;
 	type OffchainSignature = Signature;
@@ -37,5 +42,7 @@ impl pallet_identity::Config for Runtime {
 	type PendingUsernameExpiration = PendingUsernameExpiration;
 	type MaxSuffixLength = MaxSuffixLength;
 	type MaxUsernameLength = MaxUsernameLength;
-	type WeightInfo = (); // moonbeam_weights::pallet_identity::WeightInfo<Runtime>;
+	type WeightInfo = pallet_identity::weights::SubstrateWeight<Runtime>; // TODO is not recommended use default weights but currently there is a function within the
+																	   // benchmarks pallet code that cause a panic because it uses `Sr25519` signature type: https://github.com/paritytech/polkadot-sdk/blob/master/substrate/frame/identity/src/benchmarking.rs#L608
+																	   // whereas our runtime uses `Ecdsa` signature type
 }
