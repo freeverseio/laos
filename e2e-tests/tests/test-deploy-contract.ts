a4dd464e96e8dd746a413914a2ba7431c28887dd
magecnion <magecnion@gmail.com>
2024-09-23 12:29:03 +0200

    check estimate gas

--- e2e-tests/tests/test-deploy-contract.ts
+++ e2e-tests/tests/test-deploy-contract.ts
@@ -1,10 +1,13 @@
 import { describeWithExistingNode } from "./util";
-import { ALITH, ALITH_PRIVATE_KEY, BALANCES_ABI, BALANCES_BYTECODE } from "./config";
+import { ALITH, ALITH_PRIVATE_KEY, BALANCES_ABI, BALANCES_BYTECODE, GAS_LIMIT, GAS_PRICE } from "./config";
 import { expect } from "chai";
 
 describeWithExistingNode("Frontier RPC (Deploying contract)", (context) => {
     it("", async function () {
-        const contract = new context.web3.eth.Contract(BALANCES_ABI);
+        const contract = new context.web3.eth.Contract(BALANCES_ABI, null, {
+            gasPrice: GAS_PRICE,
+            gas: GAS_LIMIT,
+        });
         context.web3.eth.accounts.wallet.add(ALITH_PRIVATE_KEY);
         
         const tokenName = "FutureLAOS";
@@ -15,10 +18,13 @@ describeWithExistingNode("Frontier RPC (Deploying contract)", (context) => {
             arguments: [tokenName, tokenSymbol]
         };
         // const estimatedGas = await contract.deploy(deployOptions).estimateGas();
-        const gasPrice = (await context.web3.eth.getGasPrice()) + 1; // if we don't add +1 tx never gets included in the block
+        // const gasPrice = (await context.web3.eth.getGasPrice()) + 1; // if we don't add +1 tx never gets included in the block
         let nonce = await context.web3.eth.getTransactionCount(ALITH);
-        const result = await contract.deploy(deployOptions).send({ from: ALITH, gas: 1557906, gasPrice, nonce: nonce++ }); // en moonbeam 1861715
-        expect(context.web3.utils.isAddress(result.options.address)).to.be.eq(true);
+        const gasPrice = await context.web3.eth.getGasPrice();
+        const estimatedGas = await contract.deploy(deployOptions).estimateGas({ gas: GAS_LIMIT * 2}); // en moonbeam 1861715
+        console.log(estimatedGas);
+        // const result = await contract.deploy(deployOptions).send({ from: ALITH, gas: 1557906, gasPrice, nonce: nonce++ }); // en moonbeam 1861715
+        // expect(context.web3.utils.isAddress(result.options.address)).to.be.eq(true);
     })
 
 });
