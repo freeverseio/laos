44a23b5dc8e4ce5a4fbda286a25e66a7f308eafa
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2024-10-04 13:26:59 +0200

    added laosish ump test

--- xcm-simulator/src/lib.rs
+++ xcm-simulator/src/lib.rs
@@ -146,6 +146,7 @@ pub fn relay_ext() -> sp_io::TestExternalities {
 
 pub type RelayChainPalletXcm = pallet_xcm::Pallet<relay_chain::Runtime>;
 pub type ParachainPalletXcm = pallet_xcm::Pallet<parachain::Runtime>;
+pub type LaosishPalletXcm = pallet_xcm::Pallet<laosish::Runtime>;
 
 #[cfg(test)]
 mod tests {
@@ -205,6 +206,34 @@ mod tests {
 		);
 		ParaA::execute_with(|| {
 			assert_ok!(ParachainPalletXcm::send_xcm(
+				Here,
+				Parent,
+				Xcm(vec![Transact {
+					origin_kind: OriginKind::SovereignAccount,
+					require_weight_at_most: Weight::from_parts(INITIAL_BALANCE as u64, 1024 * 1024),
+					call: remark.encode().into(),
+				}]),
+			));
+		});
+
+		Relay::execute_with(|| {
+			use relay_chain::{RuntimeEvent, System};
+			assert!(System::events().iter().any(|r| matches!(
+				r.event,
+				RuntimeEvent::System(frame_system::Event::Remarked { .. })
+			)));
+		});
+	}
+
+	#[test]
+	fn ump_laosish() {
+		MockNet::reset();
+
+		let remark = relay_chain::RuntimeCall::System(
+			frame_system::Call::<relay_chain::Runtime>::remark_with_event { remark: vec![1, 2, 3] },
+		);
+		Laosish::execute_with(|| {
+			assert_ok!(LaosishPalletXcm::send_xcm(
 				Here,
 				Parent,
 				Xcm(vec![Transact {
