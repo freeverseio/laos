871fec5d06d7afb231a3a650a4f31159d08e3320
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2024-10-04 14:51:54 +0200

    created module test laoish

--- xcm-simulator/src/tests/mod.rs
+++ xcm-simulator/src/tests/mod.rs
@@ -1,10 +1,11 @@
-
 use super::*;
 
 use frame_support::{assert_ok, weights::Weight};
 use parity_scale_codec::Encode;
 use xcm::latest::QueryResponseInfo;
 use xcm_simulator::TestExt;
+
+mod laosish;
 
 // Helper function for forming buy execution message
 fn buy_execution<C>(fees: impl Into<Asset>) -> Instruction<C> {
@@ -55,34 +56,6 @@ fn ump() {
 	);
 	ParaA::execute_with(|| {
 		assert_ok!(ParachainPalletXcm::send_xcm(
-			Here,
-			Parent,
-			Xcm(vec![Transact {
-				origin_kind: OriginKind::SovereignAccount,
-				require_weight_at_most: Weight::from_parts(INITIAL_BALANCE as u64, 1024 * 1024),
-				call: remark.encode().into(),
-			}]),
-		));
-	});
-
-	Relay::execute_with(|| {
-		use relay_chain::{RuntimeEvent, System};
-		assert!(System::events().iter().any(|r| matches!(
-			r.event,
-			RuntimeEvent::System(frame_system::Event::Remarked { .. })
-		)));
-	});
-}
-
-#[test]
-fn ump_laosish() {
-	MockNet::reset();
-
-	let remark = relay_chain::RuntimeCall::System(
-		frame_system::Call::<relay_chain::Runtime>::remark_with_event { remark: vec![1, 2, 3] },
-	);
-	Laosish::execute_with(|| {
-		assert_ok!(LaosishPalletXcm::send_xcm(
 			Here,
 			Parent,
 			Xcm(vec![Transact {
