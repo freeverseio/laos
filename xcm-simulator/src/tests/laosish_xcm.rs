3143b7a18439380e0dd357610422ac57597bfd08
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2024-10-09 13:39:17 +0200

    laosish is able to create assets in PAraB

--- xcm-simulator/src/tests/laosish_xcm.rs
+++ xcm-simulator/src/tests/laosish_xcm.rs
@@ -107,3 +107,36 @@ fn ump_transfer_balance() {
 		);
 	});
 }
+
+#[test]
+fn xcmp_create_foreign_asset() {
+	MockNet::reset();
+
+	let para_a_native_asset_location =
+		xcm::v3::Location::new(1, [xcm::v3::Junction::Parachain(PARA_LAOSISH_ID)]);
+
+	let create_asset = parachain::RuntimeCall::ForeignAssets(ForeignAssetsCall::create {
+		id: para_a_native_asset_location,
+		admin: sibling_account_id(PARA_LAOSISH_ID),
+		min_balance: 1000,
+	});
+
+	Laosish::execute_with(|| {
+		assert_ok!(LaosishPalletXcm::send_xcm(
+			Here,
+			(Parent, Parachain(PARA_B_ID)),
+			Xcm(vec![Transact {
+				origin_kind: OriginKind::Xcm,
+				require_weight_at_most: Weight::from_parts(INITIAL_BALANCE as u64, 1024 * 1024),
+				call: create_asset.encode().into(),
+			}]),
+		));
+	});
+
+	ParaB::execute_with(|| {
+		assert!(parachain::System::events().iter().any(|r| matches!(
+			r.event,
+			parachain::RuntimeEvent::ForeignAssets(pallet_assets::Event::Created { .. })
+		)));
+	});
+}
