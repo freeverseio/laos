9cb6da35da5acd189882a7b195fc9e59cdeaed7e
luispdm <17044119+luispdm@users.noreply.github.com>
2024-10-15 13:01:57 +0200

    fix loc conv assethub

--- xcm-simulator/src/asset_hub/xcm_config/location_converter.rs
+++ xcm-simulator/src/asset_hub/xcm_config/location_converter.rs
@@ -1,7 +1,10 @@
 use super::{super::AccountId, constants::RelayNetwork};
-use xcm_builder::{AccountId32Aliases, DescribeAllTerminal, DescribeFamily, HashedDescription};
+use polkadot_parachain_primitives::primitives::Sibling;
+use xcm_builder::{AccountId32Aliases, DescribeAllTerminal, DescribeFamily, HashedDescription, ParentIsPreset, SiblingParachainConvertsVia};
 
 type LocationToAccountId = (
+	ParentIsPreset<AccountId>,
+	SiblingParachainConvertsVia<Sibling, AccountId>,
 	HashedDescription<AccountId, DescribeFamily<DescribeAllTerminal>>,
 	AccountId32Aliases<RelayNetwork, AccountId>,
 );
