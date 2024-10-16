8ac5df7b3d587b4746342e49da247c57dbaecafb
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2024-10-16 12:30:47 +0200

    fix clippy

--- runtime/laos/src/tests/metadata.rs
+++ runtime/laos/src/tests/metadata.rs
@@ -10,7 +10,7 @@ fn test_metadata_matches_golden_json() {
 	// Load the golden JSON metadata.
 	let golden_json = include_str!("metadata15.golden");
 	let golden_metadata: Value =
-		serde_json::from_str(&golden_json).expect("Failed to parse golden JSON metadata");
+		serde_json::from_str(golden_json).expect("Failed to parse golden JSON metadata");
 
 	ExtBuilder::default().build().execute_with(|| {
 		// Obtain the current metadata.
@@ -20,7 +20,7 @@ fn test_metadata_matches_golden_json() {
 		let metadata: RuntimeMetadataPrefixed = Decode::decode(&mut &bytes[..]).unwrap();
 
 		// Serialize metadata directly to a serde_json::Value
-		let metadata_value: Value = serde_json::to_value(&metadata)
+		let metadata_value: Value = serde_json::to_value(metadata)
 			.expect("Failed to serialize current metadata to JSON Value");
 
 		assert_json_eq!(metadata_value, golden_metadata);
