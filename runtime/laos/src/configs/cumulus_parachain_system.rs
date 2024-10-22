9babb14039f07cf98bf5cb344a2afa1e4bad18d8
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2024-10-22 11:11:01 +0200

    cumulus parachain system set for async backing

--- runtime/laos/src/configs/cumulus_parachain_system.rs
+++ runtime/laos/src/configs/cumulus_parachain_system.rs
@@ -38,7 +38,7 @@ impl cumulus_pallet_parachain_system::Config for Runtime {
 	type ReservedDmpWeight = ReservedDmpWeight;
 	type XcmpMessageHandler = XcmpQueue;
 	type ReservedXcmpWeight = ReservedXcmpWeight;
-	type CheckAssociatedRelayNumber = cumulus_pallet_parachain_system::RelayNumberStrictlyIncreases;
+	type CheckAssociatedRelayNumber = cumulus_pallet_parachain_system::RelayNumberMonotonicallyIncreases;
 	type ConsensusHook = ConsensusHook;
 	type DmpQueue = frame_support::traits::EnqueueWithOrigin<MessageQueue, RelayOrigin>;
 	type WeightInfo = weights::cumulus_pallet_parachain_system::WeightInfo<Runtime>;
