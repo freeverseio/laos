cf65f38dbb60896df04b19582b16f19c388d7939
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2024-10-22 11:09:08 +0200

    uninclude segmnet capacity to 1

--- runtime/laos/src/configs/cumulus_parachain_system.rs
+++ runtime/laos/src/configs/cumulus_parachain_system.rs
@@ -46,7 +46,7 @@ impl cumulus_pallet_parachain_system::Config for Runtime {
 
 /// Maximum number of blocks simultaneously accepted by the Runtime, not yet included
 /// into the relay chain.
-const UNINCLUDED_SEGMENT_CAPACITY: u32 = 3;
+const UNINCLUDED_SEGMENT_CAPACITY: u32 = 1;
 /// How many parachain blocks are processed by the relay chain per parent. Limits the
 /// number of blocks authored per slot.
 const BLOCK_PROCESSING_VELOCITY: u32 = 1;
