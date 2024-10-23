e69a3996aa351831820ddbd699b4c3786ca02bbe
magecnion <magecnion@gmail.com>
2024-10-23 16:02:30 +0200

    add exit so mocha exists when tests end

--- e2e-tests/tests/test-teleport-laos.ts
+++ e2e-tests/tests/test-teleport-laos.ts
@@ -29,26 +29,24 @@ describeWithExistingNode("Teleport Asset Hub <-> LAOS", (context) => {
 			0
 		);
 
-
 		console.log("[RELAY_CHAIN] Waiting for block production...");
 		await awaitBlockChange(apiRelaychain);
-		
+
 		console.log("[RELAY_CHAIN] Opening channels..."); // See: https://github.com/paritytech/polkadot-sdk/pull/1616
 		await sendOpenHrmpChannelTxs(apiRelaychain);
-		expect(true).to.be.false
 
-		// console.log("[ASSET_HUB] Waiting for block production...");
-		// await awaitBlockChange(apiAssetHub);
+		console.log("[ASSET_HUB] Waiting for block production...");
+		await awaitBlockChange(apiAssetHub);
 
-		// console.log("[LAOS] Waiting for block production...");
-		// await awaitBlockChange(apiLaos);
+		console.log("[LAOS] Waiting for block production...");
+		await awaitBlockChange(apiLaos);
 
-		// while (
-		// 	(await isChannelOpen(apiRelaychain, LAOS_PARA_ID, ASSET_HUB_PARA_ID)) == false ||
-		// 	(await isChannelOpen(apiRelaychain, ASSET_HUB_PARA_ID, LAOS_PARA_ID)) == false
-		// ) {
-		// 	await awaitBlockChange(apiRelaychain);
-		// }
+		while (
+			(await isChannelOpen(apiRelaychain, LAOS_PARA_ID, ASSET_HUB_PARA_ID)) == false ||
+			(await isChannelOpen(apiRelaychain, ASSET_HUB_PARA_ID, LAOS_PARA_ID)) == false
+		) {
+			await awaitBlockChange(apiRelaychain);
+		}
 	});
 
 	step("HRMP channels between Asset Hub and LAOS are open", async function () {
@@ -56,104 +54,104 @@ describeWithExistingNode("Teleport Asset Hub <-> LAOS", (context) => {
 		expect(await isChannelOpen(apiRelaychain, ASSET_HUB_PARA_ID, LAOS_PARA_ID)).to.be.true;
 	});
 
-	// step("Create LAOS foreign asset in AssetHub", async function () {
-	// 	const alith = new Keyring({ type: "ethereum" }).addFromUri(ALITH_PRIVATE_KEY);
+	step("Create LAOS foreign asset in AssetHub", async function () {
+		const alith = new Keyring({ type: "ethereum" }).addFromUri(ALITH_PRIVATE_KEY);
 
-	// 	// STEP 1: Build create foreign asset params: laosAssetId, accountId, amount
-	// 	const laosAssetId = apiAssetHub.createType("StagingXcmV3MultiLocation", {
-	// 		parents: "1",
-	// 		interior: {
-	// 			X1: { Parachain: LAOS_PARA_ID },
-	// 		},
-	// 	}) as StagingXcmV3MultiLocation;
+		// STEP 1: Build create foreign asset params: laosAssetId, accountId, amount
+		const laosAssetId = apiAssetHub.createType("StagingXcmV3MultiLocation", {
+			parents: "1",
+			interior: {
+				X1: { Parachain: LAOS_PARA_ID },
+			},
+		}) as StagingXcmV3MultiLocation;
 
-	// 	let accountId = apiAssetHub.createType("MultiAddress", laosSiblingInAssetHub) as MultiAddress;
-	// 	let amount = new BN("1000000000000000000"); // 1 LAOS
-	// 	const createCall = apiAssetHub.tx.foreignAssets.create(laosAssetId, accountId, amount);
+		let accountId = apiAssetHub.createType("MultiAddress", laosSiblingInAssetHub) as MultiAddress;
+		let amount = new BN("1000000000000000000"); // 1 LAOS
+		const createCall = apiAssetHub.tx.foreignAssets.create(laosAssetId, accountId, amount);
 
-	// 	// STEP 2: Build XCM instruction to be included in xcm.send call
-	// 	const doubleEncodedCall = apiLaos.createType("DoubleEncodedCall", {
-	// 		encoded: u8aToHex(createCall.method.toU8a()),
-	// 	}) as DoubleEncodedCall;
+		// STEP 2: Build XCM instruction to be included in xcm.send call
+		const doubleEncodedCall = apiLaos.createType("DoubleEncodedCall", {
+			encoded: u8aToHex(createCall.method.toU8a()),
+		}) as DoubleEncodedCall;
 
-	// 	const relayToken = apiLaos.createType("AssetIdV3", {
-	// 		Concrete: {
-	// 			parents: "1",
-	// 			interior: {
-	// 				Here: "",
-	// 			},
-	// 		},
-	// 	}) as AssetIdV3;
-	// 	const instruction = apiLaos.createType("XcmVersionedXcm", {
-	// 		V3: [
-	// 			{
-	// 				WithdrawAsset: [
-	// 					apiLaos.createType("MultiAssetV3", {
-	// 						id: relayToken,
-	// 						fun: apiLaos.createType("FungibilityV3", {
-	// 							Fungible: new BN("1000000000000"), // 1 DOT
-	// 						}),
-	// 					}),
-	// 				],
-	// 			},
-	// 			{
-	// 				BuyExecution: {
-	// 					fees: apiLaos.createType("MultiAssetV3", {
-	// 						id: relayToken,
-	// 						fun: apiLaos.createType("FungibilityV3", {
-	// 							Fungible: new BN("1000000000000"), // 1 DOT
-	// 						}),
-	// 					}),
-	// 					weight_limit: "Unlimited",
-	// 				},
-	// 			},
-	// 			{
-	// 				Transact: {
-	// 					originKind: apiLaos.createType("XcmOriginKind", "Xcm"),
-	// 					requireWeightAtMost: apiLaos.createType("WeightV2", {
-	// 						refTime: new BN("1000000000"), // Adjust as needed
-	// 						proofSize: new BN("5000"),
-	// 					}),
-	// 					call: doubleEncodedCall, // DoubleEncodedCall instance
-	// 				},
-	// 			},
-	// 		],
-	// 	}) as XcmVersionedXcm;
+		const relayToken = apiLaos.createType("AssetIdV3", {
+			Concrete: {
+				parents: "1",
+				interior: {
+					Here: "",
+				},
+			},
+		}) as AssetIdV3;
+		const instruction = apiLaos.createType("XcmVersionedXcm", {
+			V3: [
+				{
+					WithdrawAsset: [
+						apiLaos.createType("MultiAssetV3", {
+							id: relayToken,
+							fun: apiLaos.createType("FungibilityV3", {
+								Fungible: new BN("1000000000000"), // 1 DOT
+							}),
+						}),
+					],
+				},
+				{
+					BuyExecution: {
+						fees: apiLaos.createType("MultiAssetV3", {
+							id: relayToken,
+							fun: apiLaos.createType("FungibilityV3", {
+								Fungible: new BN("1000000000000"), // 1 DOT
+							}),
+						}),
+						weight_limit: "Unlimited",
+					},
+				},
+				{
+					Transact: {
+						originKind: apiLaos.createType("XcmOriginKind", "Xcm"),
+						requireWeightAtMost: apiLaos.createType("WeightV2", {
+							refTime: new BN("1000000000"), // Adjust as needed
+							proofSize: new BN("5000"),
+						}),
+						call: doubleEncodedCall, // DoubleEncodedCall instance
+					},
+				},
+			],
+		}) as XcmVersionedXcm;
 
-	// 	const destination = apiLaos.createType("XcmVersionedLocation", {
-	// 		V3: {
-	// 			parents: "1",
-	// 			interior: {
-	// 				X1: { Parachain: ASSET_HUB_PARA_ID },
-	// 			},
-	// 		},
-	// 	}) as XcmVersionedLocation;
+		const destination = apiLaos.createType("XcmVersionedLocation", {
+			V3: {
+				parents: "1",
+				interior: {
+					X1: { Parachain: ASSET_HUB_PARA_ID },
+				},
+			},
+		}) as XcmVersionedLocation;
 
-	// 	// STEP 3: Send the XCM instruction from Laos to Asset Hub
-	// 	const sudoCall = apiLaos.tx.sudo.sudo(apiLaos.tx.polkadotXcm.send(destination, instruction));
-	// 	await sudoCall
-	// 		.signAndSend(alith, () => {})
-	// 		.catch((error: any) => {
-	// 			console.log("transaction failed", error);
-	// 		});
+		// STEP 3: Send the XCM instruction from Laos to Asset Hub
+		const sudoCall = apiLaos.tx.sudo.sudo(apiLaos.tx.polkadotXcm.send(destination, instruction));
+		await sudoCall
+			.signAndSend(alith, () => {})
+			.catch((error: any) => {
+				console.log("transaction failed", error);
+			});
 
-	// 	// STEP 4: Check if the foreign asset was created in Asset Hub
-	// 	let waitForNBlocks = 5;
-	// 	let eventFound = null;
-	// 	while (waitForNBlocks > 0 && !eventFound) {
-	// 		const events = await apiAssetHub.query.system.events();
-	// 		events.filter((event) => {
-	// 			if (apiAssetHub.events.foreignAssets.Created.is(event.event)) {
-	// 				eventFound = event;
-	// 			}
-	// 		});
-	// 		await awaitBlockChange(apiAssetHub);
-	// 		waitForNBlocks--;
-	// 	}
-	// 	expect(eventFound.event.data[0].toString()).to.equal(laosAssetId.toString());
-	// 	expect(eventFound.event.data[1].toString()).to.equal(laosSiblingInAssetHub);
-	// 	expect(eventFound.event.data[2].toString()).to.equal(laosSiblingInAssetHub);
-	// });
+		// STEP 4: Check if the foreign asset was created in Asset Hub
+		let waitForNBlocks = 5;
+		let eventFound = null;
+		while (waitForNBlocks > 0 && !eventFound) {
+			const events = await apiAssetHub.query.system.events();
+			events.filter((event) => {
+				if (apiAssetHub.events.foreignAssets.Created.is(event.event)) {
+					eventFound = event;
+				}
+			});
+			await awaitBlockChange(apiAssetHub);
+			waitForNBlocks--;
+		}
+		expect(eventFound.event.data[0].toString()).to.equal(laosAssetId.toString());
+		expect(eventFound.event.data[1].toString()).to.equal(laosSiblingInAssetHub);
+		expect(eventFound.event.data[2].toString()).to.equal(laosSiblingInAssetHub);
+	});
 
 	// step("Teleport from LAOS to AssetHub", async function () {
 	// 	const apiLaos = await context.networks.laos;
@@ -225,8 +223,8 @@ describeWithExistingNode("Teleport Asset Hub <-> LAOS", (context) => {
 	// });
 
 	after(async function () {
-		apiAssetHub.disconnect();
-		apiLaos.disconnect();
-		apiRelaychain.disconnect();
+		await apiAssetHub.disconnect();
+		await apiLaos.disconnect();
+		await apiRelaychain.disconnect();
 	});
 });
