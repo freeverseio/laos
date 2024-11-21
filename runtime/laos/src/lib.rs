1a8748e26f517f952b7a27e3e002164571b97d43
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2024-11-21 23:43:21 +0100

    transaction_version to 1

--- runtime/laos/src/lib.rs
+++ runtime/laos/src/lib.rs
@@ -106,7 +106,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_version: 2390,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
-	transaction_version: 2,
+	transaction_version: 1,
 	state_version: 1,
 };
 
