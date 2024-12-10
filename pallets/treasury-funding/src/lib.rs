18f7a8324ef68f70031136f93d78a31d41e7c687
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2024-12-10 16:06:41 +0100

    comments refactoring

--- pallets/treasury-funding/src/lib.rs
+++ pallets/treasury-funding/src/lib.rs
@@ -19,13 +19,13 @@ pub mod pallet {
 	pub trait Config:
 		frame_system::Config + pallet_treasury::Config + pallet_vesting::Config
 	{
-		/// The overarching event type.
+		/// Event type for the runtime.
 		type RuntimeEvent: From<Event<Self>> + IsType<<Self as frame_system::Config>::RuntimeEvent>;
 
 		/// Weight information for extrinsics.
 		type WeightInfo: WeightInfo;
 
-		/// The vault account where initial funds are held.
+		/// Vault account where initial funds are held.
 		#[pallet::constant]
 		type VaultAccountId: Get<Self::AccountId>;
 	}
@@ -38,38 +38,36 @@ pub mod pallet {
 
 	#[pallet::call]
 	impl<T: Config> Pallet<T> {
-		/// Vest all funds to the vault account and then transfer all free balance
-		/// from the vault to the treasury account.
+		/// Transfers all free balance from the vault to the treasury after vesting funds.
 		///
-		/// This extrinsic:
-		/// - Requires a signed origin.
-		/// - Vests all funds to the vault account.
-		/// - Moves all free balance from the vault to the treasury.
+		/// - **Signed origin** required.
+		/// - **Actions performed**:
+		///   1. Vests all funds in the vault account.
+		///   2. Transfers the vault's free balance to the treasury.
 		///
-		/// Weight: Defined by `T::WeightInfo::fund_treasury()`.
+		/// Weight: Determined by `T::WeightInfo::fund_treasury()`.
 		#[pallet::call_index(0)]
 		#[pallet::weight(<T as Config>::WeightInfo::fund_treasury())]
 		pub fn fund_treasury(origin: OriginFor<T>) -> DispatchResultWithPostInfo {
 			let who = ensure_signed(origin)?;
 
-			// Get the configured vault account.
+			// Retrieve the vault account.
 			let vault_account = T::VaultAccountId::get();
 
-			// Vest all tokens to the vault account:
-			// `vest_other` requires a signed origin and a lookup for the beneficiary.
+			// Vest all funds to the vault account.
 			pallet_vesting::Pallet::<T>::vest_other(
 				frame_system::RawOrigin::Signed(who.clone()).into(),
 				T::Lookup::unlookup(vault_account.clone()),
 			)?;
 
-			// Retrieve the treasury account from the treasury pallet.
+			// Get the treasury account.
 			let treasury_account = pallet_treasury::Pallet::<T>::account_id();
 
-			// Determine the vault's current free balance.
+			// Get the vault's current free balance.
 			let vault_balance =
 				<T as pallet_vesting::Config>::Currency::free_balance(&vault_account);
 
-			// If there are funds in the vault, transfer them all to the treasury.
+			// Transfer all funds to the treasury if balance is positive.
 			if vault_balance > Zero::zero() {
 				<T as pallet_vesting::Config>::Currency::transfer(
 					&vault_account,
