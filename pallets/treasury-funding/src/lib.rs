8b45f39bfd64f37f1712f8e3adf423fea44b11e5
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2024-12-10 15:43:13 +0100

    compilieng

--- pallets/treasury-funding/src/lib.rs
+++ pallets/treasury-funding/src/lib.rs
@@ -6,8 +6,13 @@ pub mod weights;
 
 #[frame_support::pallet]
 pub mod pallet {
-	use frame_support::{dispatch::DispatchResultWithPostInfo, pallet_prelude::*};
+	use frame_support::{
+		dispatch::DispatchResultWithPostInfo,
+		pallet_prelude::*,
+		traits::{Currency, ExistenceRequirement},
+	};
 	use frame_system::pallet_prelude::*;
+	use sp_runtime::traits::{StaticLookup, Zero};
 
 	#[pallet::config]
 	pub trait Config:
@@ -15,6 +20,10 @@ pub mod pallet {
 	{
 		type RuntimeEvent: From<Event<Self>> + IsType<<Self as frame_system::Config>::RuntimeEvent>;
 		type WeightInfo: crate::weights::WeightInfo;
+
+		// address of where the funds are
+		#[pallet::constant]
+		type VaultAccountId: Get<Self::AccountId>;
 	}
 
 	#[pallet::pallet]
@@ -36,8 +45,32 @@ pub mod pallet {
 	impl<T: Config> Pallet<T> {
 		#[pallet::call_index(0)]
 		#[pallet::weight(Weight::from_parts(10_000, 0) + T::DbWeight::get().writes(1))]
-		pub fn do_something(origin: OriginFor<T>) -> DispatchResultWithPostInfo {
+		pub fn fund_treasury(origin: OriginFor<T>) -> DispatchResultWithPostInfo {
 			let who = ensure_signed(origin)?;
+
+			let vault_account = T::VaultAccountId::get();
+
+			// Vest all to the Vault AccountId
+			pallet_vesting::Pallet::<T>::vest_other(
+				frame_system::RawOrigin::Signed(who.clone()).into(),
+				T::Lookup::unlookup(vault_account.clone()),
+			)?;
+
+			// Retrieve the treasury account
+			let treasury_account = pallet_treasury::Pallet::<T>::account_id();
+
+			// Transfer all transferable funds from the vault to the treasury
+			let vault_balance =
+				<T as pallet_vesting::Config>::Currency::free_balance(&vault_account);
+			if vault_balance > Zero::zero() {
+				<T as pallet_vesting::Config>::Currency::transfer(
+					&vault_account,
+					&treasury_account,
+					vault_balance,
+					ExistenceRequirement::AllowDeath,
+				)?;
+			}
+
 			Self::deposit_event(Event::SomethingStored { who });
 			Ok(().into())
 		}
--- runtime/laos/src/configs/treasury_funding.rs
+++ runtime/laos/src/configs/treasury_funding.rs
@@ -1,6 +1,13 @@
-use crate::{Runtime, RuntimeEvent};
+use crate::{AccountId, Runtime, RuntimeEvent};
+use frame_support::parameter_types;
+use hex_literal::hex;
+
+parameter_types! {
+	pub TreasuryFundingVault: AccountId = hex!("9d531e3e6b0415cd79839f1fafced4822b14c23d").into();
+}
 
 impl pallet_treasury_funding::Config for Runtime {
 	type RuntimeEvent = RuntimeEvent;
 	type WeightInfo = ();
+	type VaultAccountId = TreasuryFundingVault;
 }
