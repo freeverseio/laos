d64153b3013b731e4d518522dc9bce6968703c6e
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2024-12-10 20:04:33 +0100

    set timing

--- runtime/laos/src/configs/bounties.rs
+++ runtime/laos/src/configs/bounties.rs
@@ -3,13 +3,13 @@ use crate::{
 	Balance, BlockNumber, Runtime, RuntimeEvent, Treasury,
 };
 use frame_support::parameter_types;
-use parachains_common::DAYS;
+use parachains_common::{DAYS, MINUTES};
 use sp_runtime::Permill;
 
 parameter_types! {
 	pub const BountyDepositBase: Balance = UNIT;
 	pub const BountyDepositPayoutDelay: BlockNumber = 0;
-	pub const BountyUpdatePeriod: BlockNumber = 90 * DAYS;
+	pub const BountyUpdatePeriod: BlockNumber = prod_or_fast!(7 * DAYS, 5 * MINUTES);;
 	pub const MaximumReasonLength: u32 = 16384;
 	pub const CuratorDepositMultiplier: Permill = Permill::from_percent(50);
 	pub const CuratorDepositMin: Balance = 10 * UNIT;
