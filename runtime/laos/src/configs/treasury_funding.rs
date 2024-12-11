1b1b99e5c9c6fbbb2771fb687682d615ab9481c6
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2024-12-11 15:31:59 +0100

    test fixed

--- runtime/laos/src/configs/treasury_funding.rs
+++ runtime/laos/src/configs/treasury_funding.rs
@@ -4,7 +4,7 @@ use frame_support::{parameter_types, PalletId};
 parameter_types! {
 	pub Step: u32 = 10;
 	pub MinAmountForFees: Balance = UNIT;
-	pub const TreasuryFundingPalletId: PalletId = PalletId(*b"py/trsfn");
+	pub const TreasuryFundingPalletId: PalletId = PalletId(*b"ls/trsfn");
 }
 
 impl pallet_treasury_funding::Config for Runtime {
@@ -21,7 +21,7 @@ mod tests {
 	fn check_trasury_funding_address() {
 		assert_eq!(
 			pallet_treasury_funding::Pallet::<Runtime>::account_id().to_string(),
-			"0x6D6F646C70792f747273666E0000000000000000"
+			"0x6d6f646C6c732F747273666e0000000000000000"
 		);
 	}
 }
