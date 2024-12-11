8c200e83a68a3ba8cd160238456f102dc595ed06
Tomás Senovilla Polo <tspscgs@gmail.com>
2024-12-11 12:17:38 +0100

    fmt

--- e2e-tests/tests/upgrade-chain/test-upgrade-chain.ts
+++ e2e-tests/tests/upgrade-chain/test-upgrade-chain.ts
@@ -8,13 +8,16 @@ import { CHOPSTICKS_LAOS_NODE_IP, RUNTIME_SPEC_VERSION, TARGET_PATH } from "@uti
 describeWithExistingNode(
 	"Runtime upgrade",
 	function () {
-    step("Runtime spec increases", async function(){
-      const runtime_path = path.join(TARGET_PATH, "release/wbuild/laos-runtime/laos_runtime.compact.compressed.wasm");
-      if (!existsSync(runtime_path)){
-        throw new Error (`Unable to find LAOS wasm at ${runtime_path}`);
-      }
-    });
-  },
+		step("Runtime spec increases", async function () {
+			const runtime_path = path.join(
+				TARGET_PATH,
+				"release/wbuild/laos-runtime/laos_runtime.compact.compressed.wasm"
+			);
+			if (!existsSync(runtime_path)) {
+				throw new Error(`Unable to find LAOS wasm at ${runtime_path}`);
+			}
+		});
+	},
 	// Override LAOS node ip as this test is run with chopsticks
 	CHOPSTICKS_LAOS_NODE_IP
 );
