f27e8e6644ca417e9bf38be2f724bc062dfa26f7
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2024-12-16 20:34:54 +0100

    fix version and metadata

--- runtime/laos/src/lib.rs
+++ runtime/laos/src/lib.rs
@@ -99,7 +99,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("laos"),
 	impl_name: create_runtime_str!("laos"),
 	authoring_version: 1,
-	spec_version: 2492,
+	spec_version: 2493,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,
