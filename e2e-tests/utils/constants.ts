2b2caeba19b490e4a073c88d0d251314f2bfe729
Tomás Senovilla Polo <117524919+tsenovilla@users.noreply.github.com>
2024-12-19 12:08:17 +0100

    Remove pallet treasury fork (#929)

--- e2e-tests/utils/constants.ts
+++ e2e-tests/utils/constants.ts
@@ -9,7 +9,7 @@ import ParachainStaking from "../build/contracts/ParachainStaking.sol/ParachainS
 
 // Runtime specs
 export const RUNTIME_SPEC_NAME = "laos";
-export const RUNTIME_SPEC_VERSION = 2500;
+export const RUNTIME_SPEC_VERSION = 2590;
 export const RUNTIME_IMPL_VERSION = 0;
 
 // Nodes endpoints
