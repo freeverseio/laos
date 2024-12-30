9065859bf9cc6578f4ad3e0f34a3b61440381c30
Tomás Senovilla Polo <tspscgs@gmail.com>
2024-12-30 11:45:34 +0100

    macro fmt

--- runtime/laos/src/configs/balances.rs
+++ runtime/laos/src/configs/balances.rs
@@ -28,11 +28,11 @@ parameter_types! {
 	/// In such attacks, the reset of the nonce upon account deletion can be exploited.
 	/// By setting the ExistentialDeposit to zero, we prevent the scenario where an account's
 	/// balance drops to a level that would trigger its deletion and subsequent nonce reset.
-  pub const ExistentialDeposit: Balance = 0;
-  /// For benchmark purposes, pallet balances and pallet bounties need ED to be greater than 0.
-  /// This may be removed in the future (https://github.com/paritytech/polkadot-sdk/issues/7009).
-  pub const BenchExistentialDeposit: Balance = UNIT;
-  pub const MaxLocks: u32 = 50;
+	pub const ExistentialDeposit: Balance = 0;
+	/// For benchmark purposes, pallet balances and pallet bounties need ED to be greater than 0.
+	/// This may be removed in the future (https://github.com/paritytech/polkadot-sdk/issues/7009).
+	pub const BenchExistentialDeposit: Balance = UNIT;
+	pub const MaxLocks: u32 = 50;
 	pub const MaxFreezes: u32 = 50;
 	pub const MaxHolds: u32 = 50;
 	pub const MaxReserves: u32 = 50;
