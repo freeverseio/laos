bb86b92eaf4526dc3c8aa9f80063e14f4c7f911c
Tomás Senovilla Polo <tspscgs@gmail.com>
2025-01-03 11:52:43 +0100

    Update XcmReserveTransferFilter in the simulator

--- xcm-simulator/src/laosish/configs/xcm_config.rs
+++ xcm-simulator/src/laosish/configs/xcm_config.rs
@@ -184,12 +184,12 @@ impl xcm_executor::Config for XcmConfig {
 pub type LocalOriginToLocation = SignedToAccountId20<RuntimeOrigin, AccountId, RelayNetwork>;
 pub type XcmRouter = EnsureDecodableXcm<crate::ParachainXcmRouter<MsgQueue>>;
 
-//Filter all teleports that aren't the native asset
-pub struct OnlyTeleportNative;
+//Filter all teleports/reserves that aren't the native asset
+pub struct OnlySendNative;
 impl Contains<(Location, Vec<Asset>)> for OnlyTeleportNative {
 	fn contains(t: &(Location, Vec<Asset>)) -> bool {
 		t.1.iter().all(|asset| {
-			log::trace!(target: "xcm::OnlyTeleportNative", "Asset to be teleported: {:?}", asset);
+			log::trace!(target: "xcm::OnlySendNative", "Asset to be sent out: {:?}", asset);
 			if let Asset { id: asset_id, fun: Fungible(_) } = asset {
 				asset_id.0 == HereLocation::get()
 			} else {
@@ -206,8 +206,8 @@ impl pallet_xcm::Config for Runtime {
 	type ExecuteXcmOrigin = EnsureXcmOrigin<RuntimeOrigin, LocalOriginToLocation>;
 	type XcmExecuteFilter = Nothing;
 	type XcmExecutor = XcmExecutor<XcmConfig>;
-	type XcmTeleportFilter = OnlyTeleportNative;
-	type XcmReserveTransferFilter = Nothing;
+	type XcmTeleportFilter = OnlySendNative;
+	type XcmReserveTransferFilter = OnlySendNative;
 	type Weigher = FixedWeightBounds<UnitWeightCost, RuntimeCall, MaxInstructions>;
 	type UniversalLocation = UniversalLocation;
 	type RuntimeOrigin = RuntimeOrigin;
