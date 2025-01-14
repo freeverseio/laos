219612c17125cbde54cf100f57db5740298737a4
Luigi di Girolamo <17044119+luispdm@users.noreply.github.com>
2025-01-14 14:47:07 +0100

    runtime v0.27.0 (#944)

--- e2e-tests/utils/constants.ts
+++ e2e-tests/utils/constants.ts
@@ -9,7 +9,7 @@ import ParachainStaking from "../build/contracts/ParachainStaking.sol/ParachainS
 
 // Runtime specs
 export const RUNTIME_SPEC_NAME = "laos";
-export const RUNTIME_SPEC_VERSION = 2690;
+export const RUNTIME_SPEC_VERSION = 2700;
 export const RUNTIME_IMPL_VERSION = 0;
 
 // Zombienet endpoints
