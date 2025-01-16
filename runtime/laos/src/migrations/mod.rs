55613fe92af20978ef28d83c0e1ffa53bccf5caa
Tomás Senovilla Polo <tspscgs@gmail.com>
2025-01-16 12:57:23 +0100

    Remove already applied migrations

--- runtime/laos/src/migrations/mod.rs
+++ runtime/laos/src/migrations/mod.rs
@@ -14,13 +14,4 @@
 // You should have received a copy of the GNU General Public License
 // along with LAOS.  If not, see <http://www.gnu.org/licenses/>.
 
-use crate::{ParachainSystem, Runtime};
-
-pub type Migrations = (
-	cumulus_pallet_xcmp_queue::migration::v4::MigrationToV4<Runtime>,
-	cumulus_pallet_xcmp_queue::migration::v5::MigrateV4ToV5<Runtime>,
-);
-
-impl cumulus_pallet_xcmp_queue::migration::v5::V5Config for Runtime {
-	type ChannelList = ParachainSystem;
-}
+pub type Migrations = ();
