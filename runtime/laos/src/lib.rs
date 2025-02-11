4612b7b03125ed7cc698303d88f92ee7107d9736
Luigi di Girolamo <17044119+luispdm@users.noreply.github.com>
2025-02-11 10:57:33 +0100

    runtime spec to v0.29.0 (#980)

--- runtime/laos/src/lib.rs
+++ runtime/laos/src/lib.rs
@@ -102,7 +102,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("laos"),
 	impl_name: create_runtime_str!("laos"),
 	authoring_version: 1,
-	spec_version: 2890,
+	spec_version: 2900,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,
