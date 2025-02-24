a5312de0000205f1c34053716a3a8f696d5166a2
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2025-02-24 11:32:24 +0100

    to dev version 2990

--- runtime/laos/src/lib.rs
+++ runtime/laos/src/lib.rs
@@ -106,7 +106,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("laos"),
 	impl_name: create_runtime_str!("laos"),
 	authoring_version: 1,
-	spec_version: 2900,
+	spec_version: 2990,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,
