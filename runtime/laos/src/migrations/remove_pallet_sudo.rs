04cace30a395c94f18758ce6505cf6d663cf19dc
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2025-02-24 08:46:26 +0100

    added logs

--- runtime/laos/src/migrations/remove_pallet_sudo.rs
+++ runtime/laos/src/migrations/remove_pallet_sudo.rs
@@ -5,7 +5,8 @@ pub struct Migration;
 
 impl OnRuntimeUpgrade for Migration {
 	fn on_runtime_upgrade() -> Weight {
-		let _ = migration::clear_storage_prefix(b"Sudo", b"Key", &[], None, None);
+		let result = migration::clear_storage_prefix(b"Sudo", b"Key", &[], None, None);
+		log::info!("remove pallet_sudo migration result: {:?}", result.deconstruct());
 		<Runtime as frame_system::Config>::DbWeight::get().reads_writes(0, 1)
 	}
 }
