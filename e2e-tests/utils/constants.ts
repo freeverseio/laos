063183ec3acc09a33e232d0a10e0c84bc1a31968
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2025-07-06 09:04:54 +0000

    using polkadot prefix in Hydration

--- e2e-tests/utils/constants.ts
+++ e2e-tests/utils/constants.ts
@@ -36,7 +36,7 @@ export const LAOS_ID_HYDRATION = new BN("3370");
 
 // Chain prefixes
 export const POLKADOT_PREFIX = 0;
-export const HYDRATION_PREFIX = 63;
+export const HYDRATION_PREFIX = 0;
 
 // Monetary units
 export const ONE_LAOS = new BN("1000000000000000000");
